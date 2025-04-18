e50a5314ebfb429b01f6433b9d90e66ee2df3dc6
gudo1234 <izumilitee@gmail.com>
2025-04-18 15:39:14 -0600

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,52 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+let txt = `Hola ${m.pushName} mi nombre es *${wm}* y cuento con un sistema automático con comandos que puede ayudar y buscar datos e información a través de *※ChatGpt* y mucho más.
+
+> ⁉ ғᴜɴᴄɪóɴ ɢʀᴜᴘᴏ⚙️
+${e}${s}kick *‹@υsєя›*
+${e}${s}link
+${e}${s}admins *‹τ×τ›*
+${e}${s}infogrupo
+${e}${s}tagall *‹τ×τ›*
+${e}${s}hideteg *‹τ×τ›*
+${e}${s}tag *‹rєρℓy›*
+${e}${s}icongc *‹rєρℓy›*
+${e}${s}grupo *‹αвrir/cєrrαr›*
+${e}${s}on/off
+${e}promote *‹@υsєя›*
+${e}demote *‹@υsєя›*
+
+> ⁉ ᴅᴇsᴄᴀʀɢᴀs ᴍᴜʟᴛɪᴍᴇᴅɪᴀ📂
+${e}${s}play *‹τ×τ›*
+${e}${s}play2 *‹τ×τ›*
+${e}${s}play3 *‹τ×τ›*
+${e}${s}play4 *‹τ×τ›*
+${e}${s}instagram *‹υяʟ›*
+${e}${s}tiktok *‹υяʟ›*
+${e}${s}twitter *‹υяʟ›*
+${e}${s}mediafire *‹υяʟ›*`
+await conn.sendMessage(m.chat, {
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+          forwardingScore: false,
+          externalAdReply: {
+              title: botname,
+              body: textbot,
+              thumbnailUrl: icono,
+              mediaType: 1,
+              showAdAttribution: true,
+              renderLargerThumbnail: true,
+          },
+      },
+  }, { quoted: m });
+}
+
+handler.command = ['menu', 'm']
+handler.group = true
+export default handler
