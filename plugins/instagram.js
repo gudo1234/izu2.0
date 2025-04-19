3d0f3f3efdd90474ffc1b00807ad03a30be13c3a
gudo1234 <izumilitee@gmail.com>
2025-04-18 22:22:39 -0600

    Create instagram.js

---
+++ plugins/instagram.js
@@ -0,0 +1,26 @@
+import { igdl } from 'ruhend-scraper';
+
+const handler = async (m, { args, conn }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de Instagram.`, m, null, rcanal);
+  }
+
+  try {
+    await m.react(rwait);
+    const res = await igdl(args[0]);
+    const data = res.data;
+
+    for (let media of data) {
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', ``, m);
+    await m.react(done);
+    }
+  } catch (e) {
+    return conn.reply(m.chat, `${msm} Ocurrió un error.`, m);
+    await m.react(error);
+  }
+};
+
+handler.command = ['instagram', 'ig'];
+handler.group = true;
+
+export default handler;
