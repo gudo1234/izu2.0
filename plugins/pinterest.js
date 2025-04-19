8aca6ce916bf363f23d6d5868d2f7f9b9e23ef5e
gudo1234 <izumilitee@gmail.com>
2025-04-19 09:31:35 -0600

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -4,7 +4,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) return conn.reply(m.chat,`${e} Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
 
 await m.react('🕓');
-conn.sendMessage(m.chat, { text: global.espere + `*${m.pushName}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 try {
 let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
 
