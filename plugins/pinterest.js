487644b9826e9d7d67fe8c37320692c19a427f04
gudo1234 <izumilitee@gmail.com>
2025-04-19 09:12:07 -0600

    Create pinterest.js

---
+++ plugins/pinterest.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw m.reply(`${emoji} Por favor, ingresa el link de un video/imagen de Pinterest.`);
+conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+	let ouh = await fetch(`https://api.agatz.xyz/api/pinterest?url=${text}`)
+  let gyh = await ouh.json()
+	await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*${emoji} Url:* ${gyh.data.url}`, m, null, rcanal)
+	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+}
+
+handler.command = ['pinvideo', 'pinvid', 'pin', 'pinterest']
+handler.group = true;
+
+export default handler
