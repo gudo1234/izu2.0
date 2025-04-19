0bc43131dd76ced67080c47555a59eb9c94ce317
gudo1234 <izumilitee@gmail.com>
2025-04-19 10:11:07 -0600

    Update imagen.js

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -1,21 +1,15 @@
-import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `${e} *Ejemplo de búsqueda:* ${usedPrefix + command} mia Khalifa`, m)
-const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx', 'Violet Myllers', 'Violet Myllers pussy', 'Violet Myllers desnuda', 'Violet Myllers sin ropa', 'Violet Myllers culo', 'Violet Myllers vagina', 'Pornografía', 'Pornografía infantil', 'niña desnuda', 'niñas desnudas', 'niña pussy', 'niña pack', 'niña culo', 'niña sin ropa', 'niña siendo abusada', 'niña siendo abusada sexualmente' , 'niña cogiendo', 'niña fototeta', 'niña vagina', 'hero Boku no pico', 'Mia Khalifa cogiendo', 'Mia Khalifa sin ropa', 'Mia Khalifa comiendo polla', 'Mia Khalifa desnuda']
-if (prohibited.some(word => m.text.toLowerCase().includes(word)))
-return conn.reply(m.chat, `${e} Bloqueo de búsqueda por contenido sexual.`, m)
-m.react('✖️')  
-try {
-const res = await googleImage(text)
-let image = res.getRandom()
-let link = image
-conn.sendButton(m.chat, ` ${mid.buscador}: ${text}`, wm, link, [['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`]], null, null, m )
-//conn.sendFile(m.chat, image, 'error.jpg', `${e} ${mid.buscador}: ${text}`, m, null, rcanal)
-} catch (e) {
-console.log(`error`)
-console.log(e)
-}}
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, `${e} *Ejemplo:* ${usedPrefix + command} Ghostemane`, m)
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', wm, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', wm, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', wm, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', wm, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `${e} Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+conn.sendFile(m.chat, image, 'error.jpg', `${e} ${mid.buscador}: ${text}`, m, null, rcanal)
+};
 
-handler.command = ['gimage', 'image', 'imagen', 'jpg'];
+handler.command = ['image','imagen'];
 handler.group = true;
-export default handler
+export default handler;
