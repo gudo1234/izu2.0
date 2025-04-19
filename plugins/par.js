cbb318df4727a544db63e99ef852298ff901d149
gudo1234 <izumilitee@gmail.com>
2025-04-19 15:45:44 -0600

    Add files via upload

---
+++ plugins/par.js
@@ -0,0 +1,17 @@
+import Starlights from "@StarlightsTeam/Scraper"
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+try {
+await m.react('🕓')
+let { women, man } = await Starlights.ppcouple("xd")
+await conn.sendFile(m.chat, women, 'thumbnail.jpg', `*» Chica*`, m, null, rcanal)
+await conn.sendFile(m.chat, man, 'thumbnail.jpg', `*» Chico*`, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['ppcouple']
+handler.tags = ['img']
+handler.command = ['ppcouple', 'par']
+handler.group = true;
+export default handler
