65f394b5621a59e151850544825f1a866118bd03
gudo1234 <izumilitee@gmail.com>
2025-04-19 14:39:26 -0600

    Update viewonce.js

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn }) => {
 if (!m.quoted) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
 if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
 let buffer = await m.quoted.download(false);
+m.react('🕒')
 if (/videoMessage/.test(m.quoted.mtype)) {
 return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m, null, rcanal)
 } else if (/imageMessage/.test(m.quoted.mtype)) {
