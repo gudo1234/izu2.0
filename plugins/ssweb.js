06a15a561b34a584a20dda51f50d1b7edc9f60e3
gudo1234 <izumilitee@gmail.com>
2025-04-19 14:55:07 -0600

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -4,17 +4,21 @@ if (!args[0]) return conn.reply(m.chat, `${e} Ingrese la url de una página web 
  try {
      const ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer();
         conn.sendFile(m.chat, ss, '', `${m.pushName}`, m, null, rcanal);
+  m.react('🕒')
    } catch { 
    try {  
      const ss2 = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;
         await conn.sendFile(m.chat, ss2, "Thumbnail.jpg", `${m.pushName}`, m, null, rcanal)
+ m.react('🕒')
    } catch {  
    try { 
      const ss3 =  `https://api.lolhuman.xyz/api/SSWeb?apikey=${lolkeysapi}&url=${text}`; 
         conn.sendMessage(m.chat, {image: {url: ss3}}, {quoted: m}); 
+  m.react('🕒')
    } catch { 
      const ss4 = `https://api.lolhuman.xyz/api/SSWeb2?apikey=${lolkeysapi}&url=${text}`;
         conn.sendFile(m.chat, ss4, `thumbnail.jpg`, `${m.pushName}`, m, null, rcanal)
+  m.react('🕒')
    }
   }
  }
