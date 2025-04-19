44fcd52ba196a1055406ac89c6b3035d4faca601
gudo1234 <izumilitee@gmail.com>
2025-04-19 17:19:38 -0600

    Update ia.js

--- plugins/ia.js
+++ plugins/ia.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command, text }) => {
 if (!text) return m.reply(`${e} Ejemplo de uso .${command} hola`)
 
 try {
-let api = await fetch(`https://delirius-apiofc.vercel.app/ia/chatgpt?q=${text}`)
+let api = await fetch(`https://delirius-apiofc.vercel.app/ia/gptweb?text=${text}`)
 let json = await api.json()
 m.reply(json.data)
 } catch (error) {
