78c4f0e4efeeefadf91465df1d228c69738155e5
gudo1234 <izumilitee@gmail.com>
2025-04-19 14:11:50 -0600

    Update toimg.js

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -9,12 +9,10 @@ let mime = q.mediaType || ''
 if (!/sticker/.test(mime)) throw notStickerMessage
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-conn.sendMessage(m.chat, { text: global.espere + `*${m.pushName}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await conn.sendFile(m.chat, out, 'error.png', null, m, null, rcanal)
 
 }
-handler.help = ['toimg']
-handler.tags = ['transformador']
+
 handler.command = ['toimg', 'jpg', 'jpge', 'png']
 handler.group = true;
 
