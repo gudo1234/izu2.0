4f6660899cd5d4ce0515b99c74c65e5ab4a5aa6e
gudo1234 <izumilitee@gmail.com>
2025-04-20 16:38:33 -0600

    Update perfil.js

--- plugins/perfil.js
+++ plugins/perfil.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text, usedPrefix, command: cmd }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 	let num = m.quoted?.sender || m.mentionedJid?.[0] || text
 	//if (!num) throw `*Ejemplo*: ${usedPrefix + cmd} @user`
-	if (!text) return conn.reply(m.chat, `${e} *Ejemplo*: ${usedPrefix + cmd} @user`, m)
+	if (!num) return conn.reply(m.chat, `${e} *Ejemplo*: ${usedPrefix + cmd} @user`, m)
 	num = num.replace(/\D/g, '') + '@s.whatsapp.net'
 	if (!(await conn.onWhatsApp(num))[0]?.exists) throw 'Este usuario no existe, asegurese de escribir bien el numero.'
 	let img = await conn.profilePictureUrl(num, 'image').catch(_ => './src/avatar_contact.png')
