71d6328b675d09b37641dd3bb1ddceb780798da2
gudo1234 <izumilitee@gmail.com>
2025-04-20 15:30:03 -0600

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -1,21 +1,39 @@
-import {googleIt} from '@bochilteam/scraper';
-import google from 'google-it';
-import axios from 'axios';
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-  const fetch = (await import('node-fetch')).default;
-  const text = args.join` `;
-  if (!text) return conn.reply(m.chat, `${e} *_Ingrese lo que desea buscar._*`, m);
-const url = 'https://google.com/search?q=' + encodeURIComponent(text);
-google({'query': text}).then(res => {
-let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n`
-for (let g of res) {
-teks += `_*${g.title}*_\n_${g.link}_\n_${g.snippet}_\n\n`
-} 
-const ss = `https://image.thum.io/get/fullpage/${url}`
-conn.sendFile(m.chat, ss, 'error.png', teks, m, null, rcanal)
-})
-} 
+import fetch from 'node-fetch';
 
-handler.command = ['google', 'googlef']
+let handler = async (m, { text }) => {
+  if (!text) {
+    m.reply(`${emoji} Por favor, proporciona el termino de búsqueda que deseas realizar a *Google*.`);
+    return;
+  }
+
+  const apiUrl = `https://delirius-apiofc.vercel.app/search/googlesearch?query=${encodeURIComponent(text)}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+
+    if (!result.status) {
+      m.reply('Error al realizar la búsqueda.');
+      return;
+    }
+
+    let replyMessage = `${emoji2} Resultados de la búsqueda:\n\n`;
+    result.data.slice(0, 1).forEach((item, index) => {
+      replyMessage += `☁️ *${index + 1}. ${item.title}*\n`;
+      replyMessage += `📰 *${item.description}*\n`;
+      replyMessage += `🔗 URL: ${item.url}`;
+    });
+
+m.react('✅')
+
+    m.reply(replyMessage);
+  } catch (error) {
+    console.error(`${msm} Error al realizar la solicitud a la API:`, error);
+    m.reply(`${msm} Ocurrió un error al obtener los resultados.`);
+  }
+};
+
+handler.command = ['google'];
 handler.group = true;
+
 export default handler;
