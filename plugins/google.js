1ecbf13906ed553a57d56030be04d669115a5481
gudo1234 <izumilitee@gmail.com>
2025-04-20 15:27:03 -0600

    Create google.js

---
+++ plugins/google.js
@@ -0,0 +1,21 @@
+import {googleIt} from '@bochilteam/scraper';
+import google from 'google-it';
+import axios from 'axios';
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, `${e} *_Ingrese lo que desea buscar._*`, m);
+const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+google({'query': text}).then(res => {
+let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n`
+for (let g of res) {
+teks += `_*${g.title}*_\n_${g.link}_\n_${g.snippet}_\n\n`
+} 
+const ss = `https://image.thum.io/get/fullpage/${url}`
+conn.sendFile(m.chat, ss, 'error.png', teks, m, null, rcanal)
+})
+} 
+
+handler.command = ['google', 'googlef']
+handler.group = true;
+export default handler;
