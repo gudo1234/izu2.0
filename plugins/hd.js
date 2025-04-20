1a5614138235481b6e4047588cf91e4e07a49ab2
gudo1234 <izumilitee@gmail.com>
2025-04-19 19:34:36 -0600

    Update hd.js

--- plugins/hd.js
+++ plugins/hd.js
@@ -1,45 +1,37 @@
-import FormData from "form-data";
-import Jimp from "jimp";
-const handler = async (m, {conn, usedPrefix, command}) => {
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  conn.reply(m.chat, `${e} Responda a una imagen para mejorar la calidad`, m)
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `error`
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
- } catch {
-  throw "error";
- }
-};
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["ai", "tools"];
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
+import sharp from 'sharp';
 
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
-    }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
-}
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m;
+  if (!q) {
+    return m.reply(`${e} Responde a una imagen para mejorar la calidad`);
+  }
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime.startsWith('image/')) {
+    return m.reply(`${e} Responde a una imagen para mejorar la calidad`);
+  }
+  try {
+    await m.react('⌛')
+    let media = await q.download();
+    console.log('Media downloaded successfully.');
+    const image = sharp(media);
+    const metadata = await image.metadata();
+    const newWidth = metadata.width * 2;
+    const newHeight = metadata.height * 2;
+    image.resize(newWidth, newHeight);
+    image.sharpen()
+    const buffer = await image.jpeg({ quality: 100 }).toBuffer();
+
+
+    conn.sendFile(m.chat, buffer, 'image.jpg', `${m.pushName}`, m, null, rcanal);
+    await m.react('✅')
+ ');
+  } catch (error) {
+ 
+    await m.react('❌')
+  }
+};
+
+
+handler.command = ['hd']
+handler.group = true;
+export default handler;
