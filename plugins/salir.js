628d168138d58ca54326a3d947b4b5a3f5399e5d
gudo1234 <izumilitee@gmail.com>
2025-04-21 17:04:06 -0600

    Update salir.js

--- plugins/salir.js
+++ plugins/salir.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, command }) => {
   // Establecer fecha en formato deseado: Lunes 21 de abril del 2025 a las 5:00 pm hora México
   let fechaHoraMX = moment().tz('America/Mexico_City').locale('es').format('dddd D [de] MMMM [del] YYYY [a las] h:mm a [hora México]')
 
-  m.reply(`\`\`\`Saliendo automáticamente del grupo...\`\`\`\n*Nombre:* ${groupMetadata.subject}\n*ID:* ${id}\n*Fecha de salida:* ${fechaHoraMX}`)
+  m.reply(`${e} \`Saliendo automáticamente del grupo...\`\n*Nombre:* ${groupMetadata.subject}\n*ID:* ${id}\n*${fechaHoraMX}*`)
 
   //await conn.groupLeave(id)
 }
