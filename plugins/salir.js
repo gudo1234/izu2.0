3169885c97c0ff3fadfcae5d829b9cebe895058a
gudo1234 <izumilitee@gmail.com>
2025-04-21 16:51:55 -0600

    Update salir.js

--- plugins/salir.js
+++ plugins/salir.js
@@ -1,19 +1,18 @@
-import os from 'os'
 import { getDevice } from "@whiskeysockets/baileys"
 import moment from 'moment-timezone'
-import util from 'util'
-import ws from 'ws';
-import sizeFormatter from 'human-readable'
-let MessageType =  (await import(global.baileys)).default
+
 let handler = async (m, { conn, text, command }) => {
-const fechahon = moment().tz('America/Tegucigalpa').format('DD/MM HH:mm')
-let id = text ? text : m.chat
-let txt = `${e} `\Saliendo automáticamente...`\ _\n\n*Fecha de Salida:* ${fechahon}\n*ID*: ${await conn.getName(m.chat)}`
-m.reply(txt)
-await conn.groupLeave(id)}
+  let id = text ? text : m.chat  
+  let groupMetadata = await conn.groupMetadata(id)
+  let fechaHoraMX = moment().tz('America/Mexico_City').format('DD/MM/YYYY HH:mm:ss')
+
+  m.reply(`${e}\`Saliendo automáticamente del grupo...\`\n*ID:* ${groupMetadata.subject}\n*ID:* ${id}\n*Fecha y hora (MX):* ${fechaHoraMX}`)
+
+  await conn.groupLeave(id)
+}
 
 handler.command = ['salir']
 handler.group = true
-handler.owner = true
+handler.rowner = true
 
 export default handler
