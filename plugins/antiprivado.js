bb7635741b016ac0b093990124538a9a2398fd06
gudo1234 <izumilitee@gmail.com>
2025-04-21 00:04:53 -0600

    Update antiprivado.js

--- plugins/antiprivado.js
+++ plugins/antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
        return !1
     if (!m.message)
        return !0
-    if (m.text.includes('.serbot') || m.text.includes('.serbot --code') || m.text.includes('.bots') || m.text.includes('.deletesesion') || m.text.includes('.serbot code'))
+    if (m.text.includes('.serbot') || m.text.includes('.serbot --code') || m.text.includes('.bots') || m.text.includes('.deletesesion') || m.text.includes('.code') || m.text.includes('.qr'))
        return !0
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
@@ -14,18 +14,12 @@ if (m.chat === '120363395205399025@newsletter') return !0
     if (bot.antiPrivate && !isOwner) {
 let edar = `wa.me/50492280729?text=${redes}`
 let vn = './media/ocupado.mp3'
-let txt = `,    /)🎩/)
+let str = `,    /)🎩/)
     (｡•ㅅ•｡)𖹭︩︪𝚆꯭᪶۫۫͝𝙴꯭᪶͡𝙻᪶۫۫͝𝙲꯭᪶֟፟፝͡𝙾᪶۫۫͝𝙼꯭᪶͡𝙴᪶𖹭︩︪*
     ╭∪─∪─────────❤︎₊᪲
-Hola *@${m.sender.split`@`[0]}*\n\n${e} *No está permitido usar el bot en chat privado.*\n\n> 🌟Si está interesado en mis servicios contacte a mi desarrollador, o puedes seguir el canal para mantenerte informado de las actualizaciones.`
-    this.sendButtonMessages(m.chat, [
-[txt, '𝗉𝗈𝗐𝖾𝗋𝖾𝖽 ⓒ 𝖨𝗓𝗎𝖻𝗈𝗍 𝟤𝟢𝟤𝟢-𝟤𝟢𝟤5.', icono, [
-
-], null, [
-['Desarrollador', edar], ['Seguir', channel]
-],
-[]
-]], m)
+Hola *${m.pushName}*\n\nNo está permitido usar el bot en chat privado.\n\n🚩Si está interesado en mis servicios contacte a mi desarrollador.\n wa.me/50492280729.\n\n> Puedes seguir el canal para mantenerte informado de las actualizaciones.
+    ╰────────────❤︎₊᪲`.trim()
+await this.sendFile(m.chat, icono, "Thumbnail.jpg", str.trim(), null, null, rcanal)
 await this.sendFile(m.chat, vn, 'error.mp3', null,null, true, { type: 'audioMessage', ptt: true })
        await this.updateBlockStatus(m.chat, 'block')
     }
