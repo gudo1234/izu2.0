0bdbf5766bf9d52232073507f3a657991742f8a9
gudo1234 <izumilitee@gmail.com>
2025-04-20 21:35:33 -0600

    Create calendario.js

---
+++ plugins/calendario.js
@@ -0,0 +1,39 @@
+import moment from 'moment-timezone';
+
+let handler = async (m, { conn }) => {
+    const now = moment().tz('America/Mexico_City');
+    const year = now.year();
+    const month = now.month() + 1;
+    const day = now.date();
+    const hour = now.format('hh:mm A');
+
+    let txt = `📅 *Calendario* - *${day} de ${now.format('MMMM')} del ${year}* || 🕓 *Hora:* ${hour}\n\n`;
+    txt += 'Dom Lun Mar Mié Jue Vie Sáb\n';
+
+    const firstDay = moment(`${year}-${month}-01`).day();
+    const daysInMonth = now.daysInMonth();
+
+    for (let i = 0; i < firstDay; i++) {
+        txt += '    ';
+    }
+
+    for (let i = 1; i <= daysInMonth; i++) {
+        if (i === day) {
+            txt += `[${i.toString().padStart(2, ' ')}]`;
+        } else {
+            txt += ` ${i.toString().padStart(2, ' ')}`;
+        }
+        if ((firstDay + i) % 7 === 0) {
+            txt += '\n';
+        }
+    }
+
+    txt += `\n\n🧭 *Hora actual:* ${hour}`;
+
+    let img = await (await fetch(`https://canvas.adfay.io/canvas/cute-calendar`)).buffer();
+    await conn.sendFile(m.chat, img, "calendar.jpg", txt, m, null, rcanal);
+};
+
+handler.command = ['calendario'];
+handler.group = true;
+export default handler;
