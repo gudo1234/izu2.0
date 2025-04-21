96a473f57e534905082222c26b7d06988143f97a
gudo1234 <izumilitee@gmail.com>
2025-04-20 23:25:08 -0600

    Update reglas.js

--- plugins/reglas.js
+++ plugins/reglas.js
@@ -2,7 +2,6 @@ import { getDevice } from "@whiskeysockets/baileys"
 import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-let name = await conn.getName(m.sender)
 m.react('🍉')
 let txt = `_Hola *${m.pushName}* ¿Cómo estás?_\n\n\`⚖️TÉRMINOS Y CONDICIONES DEL SERVICIO\`\n> ${e} IZUBOT, NI EL DESARROLLADOR NO SE HACE RESPONSABLE DEL USO, NÚMERO, PRIVACIDAD Y CONTENIDO MANDADO, O USADO O GESTIONADO POR USTEDES O EL BOT (usarlo bajo tu responsabilidad)\n.*[🗓]Fecha:* _${moment.tz('America/Bogota').format('DD/MM/YY')}_`
 await conn.sendMessage(m.chat, {
