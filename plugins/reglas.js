5390d543cb644fa636d7ff2ec48ce478e2199cdf
gudo1234 <izumilitee@gmail.com>
2025-04-20 23:31:54 -0600

    Update reglas.js

--- plugins/reglas.js
+++ plugins/reglas.js
@@ -3,7 +3,7 @@ import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 m.react('🍉')
-let txt = `_Hola *${m.pushName}* ¿Cómo estás?_\n\n\`⚖️TÉRMINOS Y CONDICIONES DEL SERVICIO\`\n> ${e} IZUBOT y su desarrollador no se hacen responsables por el uso, gestión, contenido compartido, privacidad ni por los números involucrados en las interacciones con el bot.\n\nEl uso del bot es completamente bajo tu propia responsabilidad. Te recomendamos usarlo de forma consciente y segura.\n.*[🗓]Fecha:* _${moment.tz('America/Bogota').format('DD/MM/YY')}_`
+let txt = `_Hola *${m.pushName}* ¿Cómo estás?_\n\n\`⚖️TÉRMINOS Y CONDICIONES DEL SERVICIO\`\n> ${e} IZUBOT y su desarrollador no se hacen responsables por el uso, gestión, contenido compartido, privacidad ni por los números involucrados en las interacciones con el bot.\n\n*El uso del bot es completamente bajo tu propia responsabilidad. Te recomendamos usarlo de forma consciente y segura.*\n.\`*[🗓]Fecha:*\` _${moment.tz('America/Bogota').format('DD/MM/YY')}_`
 await conn.sendMessage(m.chat, {
       text: txt,
       footer: textbot,
