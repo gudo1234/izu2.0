3061563a89f4848c87ddf49397dec2a0e5954504
gudo1234 <izumilitee@gmail.com>
2025-04-23 11:11:30 -0600

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -13,11 +13,12 @@ reject(err)
 })})
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) return m.reply(`${e} Ejemplo: *${usedPrefix + command}* 😎+🤑`)
+const thumbnail = await (await fetch(icono)).buffer()
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
 let stiker = await sticker(false, res.url, `${m.pushName}`)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 2, sourceUrl: redes, thumbnailUrl: redes, thumbnail}}}, { quoted: m })
 }}
 handler.help = ['emojimix *<emoji+emoji>*']
 handler.tags = ['sticker']
