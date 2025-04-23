f3ecb5d3ef9d4c5b7d0944944ed859861ef1ec68
gudo1234 <izumilitee@gmail.com>
2025-04-23 15:18:51 -0600

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -122,8 +122,9 @@ global.icono = [
 'https://files.catbox.moe/dpx2s1.jpg',
 'https://files.catbox.moe/wg1vbo.jpg',
 'https://files.catbox.moe/grk81s.jpg'].getRandom()
-  
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+
+const thumbnail = await (await fetch(icono)).buffer()
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: wm, body: textbot, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: redes, sourceUrl: redes, thumbnail, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
 
