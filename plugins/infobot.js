7eb1551b66bdb66dec2e57b2a63b091386414754
gudo1234 <izumilitee@gmail.com>
2025-04-23 16:14:15 -0600

    Update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -1,112 +1,76 @@
-import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os'
-import speed from 'performance-now'
+import { generateWAMessageFromContent } from "@whiskeysockets/baileys"
+import { cpus as _cpus, totalmem, freemem } from 'os'
+// import util from 'util'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
-import ws from 'ws'
-
 let format = sizeFormatter({
-    std: 'JEDEC',
-    decimalPlaces: 2,
-    keepTrailingZeroes: false,
-    render: (literal, symbol) => `${literal} ${symbol}B`,
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
 })
-
-let handler = async (m, { conn, usedPrefix }) => {
-   let uniqueUsers = new Map()
-
-   global.conns.forEach((conn) => {
-     if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-       uniqueUsers.set(conn.user.jid, conn)
-     }
-   })
-   let users = [...uniqueUsers.values()]
-   let totalUsers = users.length
-   let totalreg = Object.keys(global.db.data.users).length
-   let totalbots = Object.keys(global.db.data.settings).length
-   let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
-   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-   let totalchats = Object.keys(global.db.data.chats).length
-   let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length
-   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-   const used = process.memoryUsage()
-   const cpus = _cpus().map(cpu => {
-      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-      return cpu
-   })
-   const cpu = cpus.reduce((last, cpu, _, { length }) => {
-      last.total += cpu.total
-      last.speed += cpu.speed / length
-      last.times.user += cpu.times.user
-      last.times.nice += cpu.times.nice
-      last.times.sys += cpu.times.sys
-      last.times.idle += cpu.times.idle
-      last.times.irq += cpu.times.irq
-      return last
-   }, {
-      speed: 0,
-      total: 0,
-      times: {
-         user: 0,
-         nice: 0,
-         sys: 0,
-         idle: 0,
-         irq: 0
-      }
-   })
-   	let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
+let handler = async (m, { conn, usedPrefix, command }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
     }
-    let muptime = clockString(_muptime)
-   let timestamp = speed()
-   let latensi = speed() - timestamp
-let txt = `*Speed* : *${latensi.toFixed(4)} ms*\n*Grupos Registrados* : *${formatNumber(totalchats)}*\n> ${wm}`
+  })
 
-const thumbnail = await (await fetch(icono)).buffer()
-await conn.sendMessage(m.chat, {
-      text: txt,
-      footer: textbot,
-      contextInfo: {
-          mentionedJid: [m.sender],
-          isForwarded: true,
-          forwardedNewsletterMessageInfo: {
-              newsletterJid: channelRD.id,
-              newsletterName: channelRD.name,
-              serverMessageId: -1,
-          },
-          forwardingScore: false,
-          externalAdReply: {
-              title: wm,
-              body: textbot,
-              thumbnailUrl: redes,
-              thumbnail,
-              sourceUrl: redes,
-              mediaType: 1,
-              showAdAttribution: true,
-              renderLargerThumbnail: true,
-          },
-      },
-  }, { quoted: m });
+  let old = performance.now()
+  let neww = performance.now()
+  let speed = neww - old
+  let infobt = `*I N F O - I N F O R M A C I Ó N*
+  
+*ESTADO*
+Chats de grupo: *${groupsIn.length}*
+Grupos unidos: *${groupsIn.length}*
+Grupos abandonados: *${groupsIn.length - groupsIn.length}*
+Chats privados: *${chats.length - groupsIn.length}*
+Total Chats: *${chats.length}*
+Tiempo Activo: *${uptime}*
+
+
+
+🚩 *NodeJS Uso de memoria*
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+`
+const prep = generateWAMessageFromContent(m.chat, { "orderMessage": { "orderId":"6288215463787", "itemCount": 2022, "message": infobt, "orderTitle": wm, "footerText": "Nixie Bot - MD", "token": "AR6xBKbXZn0Xwmu76Ksyd7rnxI+Rx87HfinVlW4lwXa6JA==", "thumbnail": "https://telegra.ph/file/b8240f359eabfaa657286.jpg", "surface": "CATALOG" } }, { quoted: m })
+await conn.relayMessage(m.chat, prep.message,  { messageId: prep.key.id })
+
 }
 
-handler.command = ['info', 'infobot']
+handler.command = ['info', 'infobot', 'botinfo']
 
 export default handler
 
-function formatNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' D ', h, ' H ', m, ' M ', s, ' S'].map(v => v.toString().padStart(2, 0)).join('')
-}
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
