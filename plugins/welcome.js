7aed635035a8c2ef87fac42c7b82efd38a3c2ead
gudo1234 <izumilitee@gmail.com>
2025-04-24 18:44:32 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -4,10 +4,8 @@ import fetch from 'node-fetch';
 export async function before(m, { conn, participants, groupMetadata }) {
 
   if (!m.messageStubType || !m.isGroup) return true;
-if (m.quoted && m.quoted.sender) {
-        
 const thumbnail = await (await fetch(icono)).buffer()
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => thumbnail);
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => thumbnail)
   let im = await (await fetch(`${pp}`)).buffer()
   let vn = './media/a.mp3'; //welcome bendicion
 let vn2 = './media/bien.mp3'; //welcome entrada épica
