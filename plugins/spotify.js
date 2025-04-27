0491aa5e117c3d28151becf42a70da204786c214
gudo1234 <izumilitee@gmail.com>
2025-04-27 11:26:59 -0600

    Update spotify.js

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,104 +1,95 @@
-import axios from 'axios'
-import fetch from 'node-fetch'
+import { 
+  downloadTrack2,
+} from "@nechlophomeriaa/spotifydl"
+import axios from "axios"
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `${emoji} Por favor proporciona el nombre de una canción o artista.`, m)
+  try{
+    await m.react('⌛')
+    let downTrack = await downloadTrack2(`${text}`)
+    let urlspo=await spotifydl(downTrack.url)
+    if (!urlspo.status) return await m.react('❌')
+    urlspo=urlspo.download
+    let txt = `★━━━━━━━━━━━━━━━━━━━━★
+🎶 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐓𝐫𝐚𝐜𝐤 𝐃𝐨𝐰𝐥𝐨𝐚𝐝𝐞𝐫 🎶\n
+𝘼𝙧𝙩𝙞𝙨𝙩𝙖:${downTrack.artists}\n
+𝐓í𝐭𝐮𝐥𝐨:${downTrack.title}\n
+𝐃𝐮𝐫𝐚𝐜𝐢ó𝐧:${downTrack.duration}
+★━━━━━━━━━━━━━━━━━━━━★`
+  //await conn.sendMessage(m.chat, {image: {url: downTrack.imageUrl}, caption: `${caption2}`}, {quoted: m});
+    
+conn.sendFile(m.chat, downTrack.imageUrl, 'error.jpg', txt, m, null, rcanal)
 
-    if (!text) return conn.reply(m.chat, `${emoji} Por favor proporciona el nombre de una canción o artista.`, m)
-
-    try {
-        let songInfo = await spotifyxv(text)
-        if (!songInfo.length) throw `${emoji2} No se encontró la canción.`
-        let song = songInfo[0]
-        const res = await fetch(`https://archive-ui.tanakadomp.biz.id/download/spotify?url=${song.url}`)
-
-        if (!res.ok) throw `Error al obtener datos de la API, código de estado: ${res.status}`
-
-        const data = await res.json().catch((e) => { 
-            console.error('Error parsing JSON:', e)
-            throw "Error al analizar la respuesta JSON."
-        })
-
-        if (!data || !data.result || !data.result.data || !data.result.data.download) throw "No se pudo obtener el enlace de descarga."
-
-        const info = `🔥 *${data.result.data.title}*\n\n*Artista:* ${data.result.data.artis}\n*Álbum:* ${song.album}\n*Duración:* ${timestamp(data.result.data.durasi)}\n*Enlace:* ${song.url}`
-
-await conn.sendFile(m.chat, data.result.data.image, "Thumbnail.jpg", info, m, null, rcanal)
-        conn.sendMessage(m.chat, { audio: { url: data.result.data.download }, fileName: `${data.result.data.title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
-
-    } catch (e1) {
-        m.reply(`${e1.message || e1}`)
+    await conn.sendMessage(m.chat, {audio: {url: urlspo}, fileName: `${downTrack.title}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+    return await m.react('✅')
     }
+  catch
+  {
+    return await m.react('❌')
+  }
 }
 
 handler.command = ['spotify']
 handler.group = true;
-
 export default handler
 
-async function spotifyxv(query) {
-    let token = await tokens()
-    let response = await axios({
-        method: 'get',
-        url: 'https://api.spotify.com/v1/search?q=' + query + '&type=track',
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    })
-    const tracks = response.data.tracks.items
-    const results = tracks.map((track) => ({
-        name: track.name,
-        artista: track.artists.map((artist) => artist.name),
-        album: track.album.name,
-        duracion: timestamp(track.duration_ms),
-        url: track.external_urls.spotify,
-        imagen: track.album.images.length ? track.album.images[0].url : ''
-    }))
-    return results
-}
-
-async function tokens() {
-    const response = await axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64')
-        },
-        data: 'grant_type=client_credentials'
-    })
-    return response.data.access_token
-}
-
-function timestamp(time) {
-    const minutes = Math.floor(time / 60000)
-    const seconds = Math.floor((time % 60000) / 1000)
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
-}
-
-async function getBuffer(url, options) {
-    try {
-        options = options || {}
-        const res = await axios({
-            method: 'get',
-            url,
-            headers: {
-                DNT: 1,
-                'Upgrade-Insecure-Request': 1
-            },
-            ...options,
-            responseType: 'arraybuffer'
-        })
-        return res.data
-    } catch (err) {
-        return err
+async function spotifydl(url) {
+  try {
+    let maxIntentos = 10
+    let intentos = 0;
+    let statusOk = 0;
+    let res;
+    while (statusOk!==3 && statusOk!==-3 && intentos < maxIntentos) {
+      try 
+      {
+          var { data } = await axios.get('https://api.fabdl.com/spotify/get?url=' + url, {
+          headers: {
+          accept: "application/json, text/plain, */*",
+          "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+          referer: "https://spotifydownload.org/",
+          }});
+          const datax = await axios.get(`https://api.fabdl.com/spotify/mp3-convert-task/${data.result.gid}/${data.result.id}`, {
+          headers: {
+          accept: "application/json, text/plain, */*",
+          "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+          referer: "https://spotifydownload.org/",
+          }});
+          res=datax.data
+          statusOk=res.result.status
+          intentos++;
+          if (statusOk!==3 && statusOk!==-3) await new Promise(resolve => setTimeout(resolve, 3000));
+      } 
+      catch (error) {
+        return {
+          status: false,
+          message:"Error inesperado.",
+          code: 500,
+          creator:"Enigma Team"
+        };
+      }
     }
-}
-
-async function getTinyURL(text) {
-    try {
-        let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`)
-        return response.data
-    } catch (error) {
-        return text
-    }
-}
+    if(statusOk!==3) return {
+      status: false,
+      message:"Error inesperado.",
+      code: 500,
+      creator:"Enigma Team"
+    };
+    
+    return({
+    status: true,
+    title: data.result.name,
+    duration: data.result.duration_ms,
+    cover: data.result.image,
+    download: "https://api.fabdl.com" + res.result.download_url,
+    creator:"Enigma Team"
+    })
+  } 
+  catch (e) {
+  return {
+    status: false,
+    message:"Error inesperado.",
+    code: 500,
+    creator:"Enigma Team"
+  }}
+           }
