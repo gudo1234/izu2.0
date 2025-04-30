e39d49b872f05ff77cf9cca6710d2ce0fb731186
gudo1234 <izumilitee@gmail.com>
2025-04-30 12:16:15 -0600

    Update take.js

--- plugins/take.js
+++ plugins/take.js
@@ -1,26 +1,26 @@
-import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, `${e} Responda a un sticker para personalizarlo`, m)
-  let stiker = false
-  const thumbnail = await (await fetch(icono)).buffer()
-  try {
-    let [packname, ...author] = text.split('|')
-    author = (author || []).join('|')
-    let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw `${e} *_Responde a un sticker_*`
-    let img = await m.quoted.download()
-    if (!img) throw `${e} *_Responde a un sticker_*`
-    stiker = await addExif(img, packname || '', author || '')
-  } catch (e) {
-    console.error(e)
-    if (Buffer.isBuffer(e)) stiker = e
-  } finally {
-  if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 1, sourceUrl: redes, thumbnailUrl: redes, thumbnail}}}, { quoted: m })
-    else throw '⚠️ *_La conversión falló._*'
-  }
-}
+import { addExif } from '../lib/sticker.js';
+import { sticker } from '../lib/sticker.js';
+import fetch from 'node-fetch';
+import axios from 'axios';
 
-handler.command = ['take', 'robar', 'wm']
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!m.quoted) return m.reply(`${e} Por favor, responde a un sticker con el comando *${usedPrefix + command}* seguido del nuevo nombre.\nEjemplo: *${usedPrefix + command} Nuevo Nombre*`);
+const thumbnail = await (await fetch(icono)).buffer()
+  const sticker = await m.quoted.download();
+  if (!sticker) return m.reply(`${emoji2} No se pudo descargar el sticker.`);
+
+  const textoParts = text.split(/[\u2022|]/).map(part => part.trim());
+  const userId = m.sender;
+  let packstickers = global.db.data.users[userId] || {};
+  let texto1 = textoParts[0] || packstickers.text1 || `${m.pushName}`;
+  let texto2 = textoParts[1] || packstickers.text2;
+
+  const exif = await addExif(sticker, texto1, texto2);
+
+  await conn.sendFile(m.chat, exif, 'wm.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 1, sourceUrl: redes, thumbnailUrl: redes, thumbnail}}}, { quoted: m })
+};
+
+handler.command = ['take', 'robar', 'wm'];
 handler.group = true;
 
-export default handler
+export default handler;
