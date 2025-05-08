4cbccad00278fe18d2fca1d3da8f9c9e5aeb8a7b
gudo1234 <izumilitee@gmail.com>
2025-05-08 17:16:39 -0600

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -68,7 +68,7 @@ global.db.data.users[m.sender].Subs = new Date * 1
 } 
 handler.help = ['qr', 'code']
 handler.tags = ['serbot']
-handler.command = ['q', 'co']
+handler.command = ['qr', 'code']
 export default handler 
 
 export async function yukiJadiBot(options) {
