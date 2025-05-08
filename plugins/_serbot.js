51ba03a07d131cff8f3eb433eba907605efad801
gudo1234 <izumilitee@gmail.com>
2025-05-08 17:18:35 -0600

    Update _serbot.js

--- plugins/_serbot.js
+++ plugins/_serbot.js
@@ -1,8 +1,8 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+/*let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   conn.reply(m.chat, `${e} Comando deshabilitado por el desarrollador.`, m)
 }
 
 handler.command = ['qr', 'code']
 handler.group = true
-export default handler
+export default handler*/
