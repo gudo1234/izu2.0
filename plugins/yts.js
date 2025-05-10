e57a16c573ccda64aede4df135c50b8e4f1ce0e7
gudo1234 <izumilitee@gmail.com>
2025-05-10 07:29:36 -0600

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -24,6 +24,7 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
 📌 *¿Cómo descargar?*  
 ${e} Cada resultado tiene un número (#1, #2, #3...).  
 Responde a este mensaje usando ese número para elegir qué descargar:
+
 ━━━━━━━━━━━━━
 ✑ \`a 1\` o \`audio 1\` → Audio
 ✑ \`v 1\` o \`video 1\` → Video
