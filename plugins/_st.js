7d4b2cafbf22d65c0cc90d85e3062631a4ed37a3
gudo1234 <izumilitee@gmail.com>
2025-05-14 12:40:02 -0600

    Update _st.js

--- plugins/_st.js
+++ plugins/_st.js
@@ -32,7 +32,32 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     img = await fetch(args[0]).then(res => res.buffer());
     mime = 'image/url';
   } else {
-    return conn.reply(m.chat, `Responde a una imagen o video/gif para generar un sticker o agrega una opción de forma.
+    return conn.reply(m.chat, `${e} Responde a una imagen o video/gif para generar un sticker o agrega una opción de forma.
+
+┌───────────
+│ 🧪 Tipo: Sticker personalizado
+│ ⚙ \`Formas disponibles:\`
+│  ● *Básicas:*
+│ ├─ -c ⟶ Circular
+│ ├─ -t ⟶ Triangular
+│ ├─ -d ⟶ Diamante
+│ ├─ -h ⟶ Hexágono
+│ └─ -p ⟶ Pentágono
+│  ● *Decorativas:*
+│ ├─ -a ⟶ Corazón
+│ ├─ -b ⟶ Burbuja
+│ ├─ -l ⟶ Hoja
+│ ├─ -n ⟶ Luna
+│ ├─ -s ⟶ Estrella
+│ └─ -z ⟶ Rayo
+│  ● *Especiales:*
+│ ├─ -r ⟶ Curvado
+│ ├─ -e ⟶ Esquinas redondeadas
+│ ├─ -m ⟶ Espejo
+│ ├─ -f ⟶ Flecha
+│ ├─ -x ⟶ Acoplado
+│ └─ -i ⟶ Ampliado
+└───────────
 
 ◈ Usa \`${usedPrefix + command} -a\` respondiendo a una imagen o video.`, m);
   }
