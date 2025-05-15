756841b2b3274f95af994bc53b67560d83a0835f
gudo1234 <izumilitee@gmail.com>
2025-05-14 21:52:14 -0600

    Update xvideosdl.js

--- plugins/xvideosdl.js
+++ plugins/xvideosdl.js
@@ -21,7 +21,7 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     }
 };
 
-handler.command = ['xvideosdl', 'xvideos'];
+handler.command = ['xvideosdl'];
 handler.group = true;
 
 export default handler;
