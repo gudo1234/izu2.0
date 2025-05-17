42b5d1950fd84bde1d7755857ef5a0411c9b909e
gudo1234 <izumilitee@gmail.com>
2025-05-17 11:46:38 -0600

    Update imagen.js

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -4,9 +4,6 @@ if (!text) return conn.reply(m.chat, `${e} *Ejemplo:* ${usedPrefix + command} Gh
 const res = await googleImage(text);
 const image = await res.getRandom();
 const link = image;
-const messages = [['Imagen 1', wm, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', wm, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', wm, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', wm, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-//await conn.sendCarousel(m.chat, `${e} Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
 conn.sendFile(m.chat, await res.getRandom(), 'error.jpg', ``, m, null, rcanal)
 conn.sendFile(m.chat, await res.getRandom(), 'error.jpg', ``, m)
 conn.sendFile(m.chat, await res.getRandom(), 'error.jpg', ``, m)
