37f19fca26e30c57adf437caf356489818258ce0
gudo1234 <izumilitee@gmail.com>
2025-05-16 23:57:32 -0600

    Update triste.js

--- plugins/triste.js
+++ plugins/triste.js
@@ -2,12 +2,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 let vn = './media/prueba.mp3'
 let vn2 = './media/prueba2.mp3'
-conn.sendFile(m.chat, [vn, vn2].getRandom(), 'a.mp3', null, m, true, { 
+let vn3 = './media/snow.mp3'
+conn.sendFile(m.chat, [vn, vn2, vn3].getRandom(), 'a.mp3', null, m, true, { 
 type: 'audioMessage', 
 ptt: true 
 })
 }
 
-handler.customPrefix = /💔|😢|😭|😞|😔|😟|😫|😩|🥺|🙁|😣|😖|😿/
+handler.customPrefix = /💔|😢|😭|😞|😔|😟|😫|😩|🥺|🙁|😣|😖|😿|🙁/
 handler.command = new RegExp
 export default handler
