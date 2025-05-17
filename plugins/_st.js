23dd9bd3e70607d98e7d46488bc71b5f9c2b77b6
gudo1234 <izumilitee@gmail.com>
2025-05-17 08:42:22 -0600

    Update _st.js

--- plugins/_st.js
+++ plugins/_st.js
@@ -2,6 +2,7 @@ import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
@@ -12,7 +13,7 @@ let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`¡El video no puede durar mas de 8 segundos!`)
 let img = await q.download?.()
-
+const thumbnail = await (await fetch(icono)).buffer();
 if (!img) return conn.reply(m.chat, `${e} Por favor, envia una imagen o video para hacer un sticker.`, m )
 
 
@@ -40,7 +41,7 @@ else return m.reply(`${e} El url es incorrecto`)
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 2, sourceUrl: redes, thumbnail: icono, thumbnailUrl: redes}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${m.pushName}`, body: textbot, mediaType: 2, sourceUrl: redes, thumbnail, thumbnailUrl: redes}}}, { quoted: m })
 
 else return conn.reply(m.chat, `${e} Por favor, envia una imagen o video para hacer un sticker.`, m)
 
