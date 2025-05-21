bafe37582c5b8aa9b45d109288d2a8a8fde53545
gudo1234 <izumilitee@gmail.com>
2025-05-20 19:58:57 -0600

    Update _queso.js

--- plugins/_queso.js
+++ plugins/_queso.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn}) => {
 if (command == 'que', 'q', 'ke', 'kee', 'quee') {
-let txt = `zo🧀`}
-//m.reply(`zo🧀`)}
-if (command == 'a', 'aa', 'aaa', 'ah', 'ha', 'haa', 'ahh') {
+let txt = `zo🧀`
+m.reply(`zo🧀`)}
+/*if (command == 'a', 'aa', 'aaa', 'ah', 'ha', 'haa', 'ahh') {
 txt += `arroz`}
 //}
 if (command == '🫩') {
 txt += `Mucha paja vro`}
-//}
+//}*/
 conn.sendMessage(m.chat, { text: txt }, { quoted: m})
 }
 
