b4c59a847bdd05c0f3ae66f36fdd291b985893df
gudo1234 <izumilitee@gmail.com>
2025-05-20 19:02:01 -0600

    Update _queso.js

--- plugins/_queso.js
+++ plugins/_queso.js
@@ -2,18 +2,18 @@ let handler = async (m, { conn, text }) => {
   const lower = text?.toLowerCase()?.trim()
   let response
 
-  if (lower === 'a') {
+  if (/^a+$/.test(lower)) {
     response = 'arroz'
-  } else if ([
-    'q', 'k', 'qe', 'ke', 'que', 'keso', 'queso',
-    'quee', 'kee', 'qe', 'queee', 'keee', 'kso', 'ksooo', 'ksooo'
-  ].includes(lower)) {
+  } else if (/^(q+|k+|qe+|ke+|que+|k+e+|keso+|queso+)$/i.test(lower)) {
     response = 'queso'
   } else {
     response = '🧀zo'
   }
 
-  await conn.sendMessage(m.chat, { text: response }, { quoted: m, ...rcanal })
+  await conn.sendMessage(m.chat, { text: response }, { quoted: m})
 }
 
+// Se activa con mensajes que comienzan con cualquiera de estos patrones
+handler.customPrefix = /^(a+|que+|qe+|ke+|k+|q+)$/i
+handler.command = new RegExp // necesario para handlers con customPrefix
 export default handler
