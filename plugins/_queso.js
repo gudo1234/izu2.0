202df30788839b87d7ddc507c0c08d692043e0de
gudo1234 <izumilitee@gmail.com>
2025-05-20 19:55:32 -0600

    Update _queso.js

--- plugins/_queso.js
+++ plugins/_queso.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn}) => {
 if (command == 'que', 'q', 'ke', 'kee', 'quee') {
 let txt = `zo🧀`}
-/*//m.reply(`zo🧀`)}
+//m.reply(`zo🧀`)}
 if (command == 'a', 'aa', 'aaa', 'ah', 'ha', 'haa', 'ahh') {
 txt += `arroz`}
 //}
 if (command == '🫩') {
 txt += `Mucha paja vro`}
-//}*/
+//}
 conn.sendMessage(m.chat, { text: txt }, { quoted: m})
 }
 
-handler.customPrefix = /^['que', 'q', 'ke', 'kee', 'quee', 'a', 'aa', 'aaa', 'ah', 'ha', 'haa', 'ahh', '🫩']$/i
+handler.customPrefix = /^(que|q|ke|kee|quee|a|aa|aaa|ah|ha|haa|ahh|🫩)$/i
 handler.command = new RegExp
 export default handler
