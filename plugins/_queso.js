e5bfaaeb9fcc654eb355e6be33cd8af7873a1b10
gudo1234 <izumilitee@gmail.com>
2025-05-20 20:45:59 -0600

    Update _queso.js

--- plugins/_queso.js
+++ plugins/_queso.js
@@ -1,12 +1,3 @@
-/*let handler = async (m, { conn }) => {
-  let txt = /^(a)$/i.test(m.text) ? 'arroz' : 'zoo🧀'
-  await conn.sendMessage(m.chat, { text: txt }, { quoted: m })
-}
-
-handler.customPrefix = /^(Que|que|qe|ke|Qe|k|Ke|Kee|Quee|a)$/i
-handler.command = new RegExp
-export default handler*/
-
 let handler = async (m, { conn }) => {
   let txt = /^(a)$/i.test(m.text) ? 'arroz' : 'zoo🧀'
   await conn.sendMessage(
