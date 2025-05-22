2c5676646cd20d47920e483c6bef6cda557cc4a8
gudo1234 <izumilitee@gmail.com>
2025-05-21 22:28:55 -0600

    Update triste.js

--- plugins/triste.js
+++ plugins/triste.js
@@ -1,13 +1,24 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
+  let vn = './media/prueba.mp3'
+  let vn2 = './media/prueba2.mp3'
+  let vn3 = './media/snow.mp3'
+  let vn4 = './media/sad.mp3'
 
-let vn = './media/prueba.mp3'
-let vn2 = './media/prueba2.mp3'
-let vn3 = './media/snow.mp3'
-let vn4 = './media/sad.mp3'
-conn.sendFile(m.chat, [vn, vn2, vn3, vn4].getRandom(), 'a.mp3', null, m, true, { 
-type: 'audioMessage', 
-ptt: true 
-})
+  await conn.sendMessage(m.chat, {
+    audio: { url: [vn, vn2, vn3, vn4].getRandom() },
+    ptt: true,
+    mimetype: 'audio/mpeg',
+    fileName: 'audio.mp3',
+    seconds: 9999,
+    contextInfo: {
+      forwardingScore: 999,
+      isForwarded: true
+    }
+  }, {
+    quoted: m,
+    ephemeralExpiration: 24 * 60 * 60, // 24 horas en segundos
+    disappearingMessagesInChat: 24 * 60 * 60
+  })
 }
 
 handler.customPrefix = /💔|😢|😭|😞|😔|😟|😫|😩|🥺|🙁|😣|😖|😿|🙁/
