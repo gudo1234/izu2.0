33c503ee640b17a0ac5a0da0347db9eb6d151a54
gudo1234 <izumilitee@gmail.com>
2025-05-22 14:52:42 -0600

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -1,23 +1,15 @@
-let linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
+var handler = async (m, { conn, args }) => {
 
-let handler = async (m, { conn, text, isOwner }) => {
-    if (!text) return m.reply(`${emoji} Debes enviar una invitacion para que *${botname}* se una al grupo.`);
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+conn.reply(m.chat, `${e} *Enlace de invitación del grupo*\n` + link, null, m, { detectLink: true })
 
-    let [_, code] = text.match(linkRegex) || [];
+}
+handler.help = ['link']
+handler.tags = ['grupo']
+handler.command = ['link', 'linkgc']
 
-    if (!code) return m.reply(`${emoji2} Enlace de invitación no válido.`);
+handler.group = true
+handler.botAdmin = true
 
-    if (isOwner) {
-        await conn.groupAcceptInvite(code)
-            .then(res => m.reply(`${emoji} Me he unido exitosamente al grupo.`))
-            .catch(err => m.reply(`${msm} Error al unirme al grupo.`));
-    } else {
-        let message = `${emoji} Invitación a un grupo:\n${text}\n\nPor: @${m.sender.split('@')[0]}`;
-        await conn.sendMessage(`${suittag}` + '@s.whatsapp.net', { text: message, mentions: [m.sender] }, { quoted: m });
-        m.reply(`${emoji} El link del grupo ha sido enviado a mi propietario, luego revivirá una respuesta`);
-    }
-};
-
-handler.command = ['invite', 'join', 'entra', 'entrabot'];
-
-export default handler;
+export default handler
