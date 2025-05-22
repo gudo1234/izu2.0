5cb482da52bc793c3b21b0e3f85453979620731f
gudo1234 <izumilitee@gmail.com>
2025-05-22 09:18:21 -0600

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,35 +1,39 @@
-import fetch from 'node-fetch';
-function handler(m) {
-const thumbnail = await (await fetch(icono)).buffer()
-const emojis = ['🍎', '🍒', '🍉', '🍊', '🍋', '🍏', '🍌', '🍍', '🍓', '🍇', '🍈', '🍒', '🍑', '🥭', '🍐', '🥥', '🍋‍🟩'];
-    for (let i = 0; i < emojis.length; i++) {
-        setTimeout(async () => {
-            await m.react(emojis[i]);
-        }, i * 1000);
-    }
-conn.sendMessage(m.chat, {
-  contacts: {
-    contacts: [{
-      displayName: author,
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Edar;;;\nFN:―͟͞🍁͓̽𝆥⟅ₑᵤ⟆𝇃𝇄⳻݊͜⳺𝇄𝇃\nORG:izuBot Owner\nTITLE: Developer\nTEL;type=CELL;type=VOICE;waid=50492280729:+504 9228 0727\nTEL;type=WORK;type=VOICE:+504 9228 0729\nEMAIL: izumilitee@gmail.com\nADR;type=WORK:;;Por el dia no hago nada y por la tarde descanso;;;;\nURL:https://www.instagram.com/edar504__\nNOTE:xd.\nBDAY:2025-12-31\nPHOTO;VALUE=URI:https://mystickermania.com/cdn/stickers/cute/mochi-peach-cat-bread-512x512.png\nEND:VCARD`
-    }]
-  },
- contextInfo: {
-"externalAdReply": {
-"renderLargerThumbnail": true,
-"mediaType": 1,
-"title": 'No molestar, xD -_-',
-"body": wm,
-"thumbnailUrl": redes,
-"thumbnail": thumbnail,
-sourceUrl: redes
-}
-}
-}, {
-  quoted: m
-});
-}
-handler.customPrefix = /^(Edar|edar|@50492280729|.owner|owner|.dueño|dueño|.creador|creador)$/i
-handler.command = new RegExp 
+import axios from 'axios';
 
-export default handler
+async function handler(m, { conn }) {
+  const { data: thumbnail } = await axios.get(icono, { responseType: 'arraybuffer' });
+
+  // Reacciones con emojis
+  const emojis = ['🍎', '🍒', '🍉', '🍊', '🍋', '🍏', '🍌', '🍍', '🍓', '🍇', '🍈', '🍒', '🍑', '🥭', '🍐', '🥥', '🍋‍🟩'];
+  for (let i = 0; i < emojis.length; i++) {
+    setTimeout(async () => {
+      await m.react(emojis[i]);
+    }, i * 1000);
+  }
+
+  // Enviar contacto con vista enriquecida
+  conn.sendMessage(m.chat, {
+    contacts: {
+      contacts: [{
+        displayName: author,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Edar;;;\nFN:―͟͞🍁͓̽𝆥⟅ₑᵤ⟆𝇃𝇄⳻݊͜⳺𝇄𝇃\nORG:izuBot Owner\nTITLE: Developer\nTEL;type=CELL;type=VOICE;waid=50492280729:+504 9228 0727\nTEL;type=WORK;type=VOICE:+504 9228 0729\nEMAIL: izumilitee@gmail.com\nADR;type=WORK:;;Por el dia no hago nada y por la tarde descanso;;;;\nURL:${redes}\nNOTE:xd.\nBDAY:2025-12-31\nPHOTO;VALUE=URI:${icono}\nEND:VCARD`
+      }]
+    },
+    contextInfo: {
+      externalAdReply: {
+        renderLargerThumbnail: true,
+        mediaType: 1,
+        title: 'No molestar, xD -_-',
+        body: wm,
+        thumbnailUrl: redes, // Esta es la URL directa
+        thumbnail,           // Este es el buffer de la imagen
+        sourceUrl: redes     // URL de destino si dan clic
+      }
+    }
+  }, { quoted: m });
+}
+
+handler.customPrefix = /^(Edar|edar|@50492280729|.owner|owner|.dueño|dueño|.creador|creador)$/i;
+handler.command = new RegExp;
+
+export default handler;
