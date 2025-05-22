5de06125fad282a597652730295d115632566ba6
gudo1234 <izumilitee@gmail.com>
2025-05-21 22:22:04 -0600

    Update triste.js

--- plugins/triste.js
+++ plugins/triste.js
@@ -3,7 +3,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 let vn = './media/prueba.mp3'
 let vn2 = './media/prueba2.mp3'
 let vn3 = './media/snow.mp3'
-conn.sendFile(m.chat, [vn, vn2, vn3].getRandom(), 'a.mp3', null, m, true, { 
+let vn4 = './media/sad.mp3'
+conn.sendFile(m.chat, [vn, vn2, vn3, vn4].getRandom(), 'a.mp3', null, m, true, { 
 type: 'audioMessage', 
 ptt: true 
 })
