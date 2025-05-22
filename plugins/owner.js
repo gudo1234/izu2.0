375973ff59ef42dbbd2d67af0a82a97c006a6167
gudo1234 <izumilitee@gmail.com>
2025-05-22 09:07:08 -0600

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ conn.sendMessage(m.chat, {
   contacts: {
     contacts: [{
       displayName: author,
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Edar;;;\nFN:${author}\nORG:izuBot Owner\nTITLE: Developer\nTEL;type=CELL;type=VOICE;waid=50492280729:+504 9228 0727\nTEL;type=WORK;type=VOICE:+504 9228 0729\nEMAIL: izumilitee@gmail.com\nADR;type=WORK:;;Por el dia no hago nada y por la tarde descanso;;;;\nURL:https://www.instagram.com/edar504__\nNOTE:xd.\nBDAY:2025-12-31\nPHOTO;VALUE=URI:https://mystickermania.com/cdn/stickers/cute/mochi-peach-cat-bread-512x512.png\nEND:VCARD`
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Edar;;;\nFN:―͟͞🍁͓̽𝆥⟅ₑᵤ⟆𝇃𝇄⳻݊͜⳺𝇄𝇃\nORG:izuBot Owner\nTITLE: Developer\nTEL;type=CELL;type=VOICE;waid=50492280729:+504 9228 0727\nTEL;type=WORK;type=VOICE:+504 9228 0729\nEMAIL: izumilitee@gmail.com\nADR;type=WORK:;;Por el dia no hago nada y por la tarde descanso;;;;\nURL:https://www.instagram.com/edar504__\nNOTE:xd.\nBDAY:2025-12-31\nPHOTO;VALUE=URI:https://mystickermania.com/cdn/stickers/cute/mochi-peach-cat-bread-512x512.png\nEND:VCARD`
     }]
   },
  contextInfo: {
