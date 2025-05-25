052847d497a6d83118e4429b5ce084f2729ca539
gudo1234 <izumilitee@gmail.com>
2025-05-24 23:42:05 -0600

    Update _xd.js

--- plugins/_xd.js
+++ plugins/_xd.js
@@ -1,4 +1,4 @@
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 
 function pickRandom(list) {
@@ -24,60 +24,6 @@ const handler = async (m, { conn }) => {
     )
   } catch (e) {
     console.error('Error al enviar sticker xd:', e)
-  }
-}
-
-handler.customPrefix = /xd/
-handler.command = new RegExp
-
-export default handler*/
-
-import fetch from 'node-fetch'
-import { sticker } from '../lib/sticker.js'
-import cheerio from 'cheerio'
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
-
-async function buscarImagenXD() {
-  const query = 'xd meme'
-  const url = `https://duckduckgo.com/?q=${encodeURIComponent(query)}&iax=images&ia=images`
-
-  const res = await fetch(url, {
-    headers: { 'User-Agent': 'Mozilla/5.0' }
-  })
-  const html = await res.text()
-
-  const $ = cheerio.load(html)
-  const scripts = $('script:not([src])').get()
-  const scriptWithData = scripts.find(script => $(script).html().includes('var o'))
-
-  if (!scriptWithData) return null
-
-  const match = $(scriptWithData).html().match(/"image":"(.*?)"/g)
-  if (!match) return null
-
-  const urls = match.map(e => e.replace(/"image":"(.*?)"/, '$1').replace(/\\u002F/g, '/'))
-  return pickRandom(urls)
-}
-
-const handler = async (m, { conn }) => {
-  try {
-    const imageUrl = await buscarImagenXD()
-    if (!imageUrl) throw new Error('No se pudo obtener imagen XD')
-
-    const imgBuffer = await fetch(imageUrl).then(res => res.buffer())
-    const webpBuffer = await sticker(imgBuffer, false, global.packname, global.author)
-
-    await conn.sendMessage(
-      m.chat,
-      { sticker: webpBuffer },
-      { quoted: m }
-    )
-  } catch (e) {
-    console.error('Error al enviar sticker xd:', e)
-    await m.reply('No pude generar un sticker XD en este momento.')
   }
 }
 
