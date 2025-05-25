0c6fe1b24ea8f1257c5de62fa179344d92ca5ed1
gudo1234 <izumilitee@gmail.com>
2025-05-24 19:36:30 -0600

    Update _edi.js

--- plugins/_edi.js
+++ plugins/_edi.js
@@ -1,62 +1,3 @@
-/*import { getDevice } from "@whiskeysockets/baileys"
-import axios from 'axios';
-import PhoneNum from 'awesome-phonenumber';
-import moment from 'moment-timezone';
-import 'moment/locale/es.js';
-
-moment.locale('es');
-
-const regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
-
-function banderaEmoji(countryCode) {
-  if (!countryCode || countryCode.length !== 2) return '';
-  const codePoints = [...countryCode.toUpperCase()]
-    .map(char => 0x1F1E6 + char.charCodeAt(0) - 65);
-  return String.fromCodePoint(...codePoints);
-}
-
-async function handler(m, { conn }) {
-  const jid = m.sender;
-  const number = jid.split('@')[0];
-  const name = await conn.getName(jid);
-
-  const phoneInfo = PhoneNum('+' + number);
-  const countryCode = phoneInfo.getRegionCode('international');
-  const countryName = regionNames.of(countryCode) || 'Desconocido';
-  const emojiBandera = banderaEmoji(countryCode);
-
-  let capital = 'Desconocida';
-  let fechaLocal = 'No disponible';
-
-  try {
-    const res = await axios.get(`https://restcountries.com/v3.1/alpha/${countryCode}`);
-    const data = res.data[0];
-    capital = data.capital?.[0] || 'Desconocida';
-
-    const zonaHoraria = data.timezones?.[0];
-    if (zonaHoraria) {
-      const now = moment().tz(zonaHoraria);
-      fechaLocal = now.format('dddd, D [de] MMMM [de] YYYY');
-    }
-  } catch (e) {
-    console.error('Error al obtener datos del país:', e);
-  }
-
-  const info = `
-*Nombre:* ${name}
-*Número:* +${number}
-*País:* ${countryName} ${emojiBandera}
-*Capital:* ${capital}
-*Fecha:* ${fechaLocal}
-*Sistema/Opr:* ${getDevice(m.key.id)}
-  `.trim();
-
-  m.reply(info);
-}
-
-handler.command = ['edi'];
-export default handler;*/
-
 import axios from 'axios'
 import PhoneNum from 'awesome-phonenumber'
 import moment from 'moment-timezone'
@@ -115,7 +56,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   let bio = await conn.fetchStatus(target).catch(_ => null)
   let business = await conn.getBusinessProfile(target).catch(_ => null)
 
-  let caption = `*Información de WhatsApp*\n\n`
+  let caption = `${e} *Información de WhatsApp*\n\n`
   caption += `*Nombre:* ${name || '-'}\n`
   caption += `*Número:* ${formatNum}\n`
   caption += `*País:* ${country} ${flag}\n`
@@ -128,7 +69,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   caption += `*Sistema/Device:* ${own ? getDevice(m.key.id) : '-'}\n`
 
   if (business) {
-    caption += `\n*Cuenta Business:*\n`
+    caption += `\n⚡ *Cuenta Business:*\n`
     caption += `*ID:* ${business.wid}\n`
     caption += `*Sitio Web:* ${business.website || '-'}\n`
     caption += `*Email:* ${business.email || '-'}\n`
@@ -145,5 +86,6 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }, { quoted: m })
 }
 
-handler.command = ['edi']
+handler.command = ['wastalk', 'perfil', 'ava'];
+handler.group = true;
 export default handler
