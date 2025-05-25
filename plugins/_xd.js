eb02a3b1833e49d162bd3dd974719e8cccef16d1
gudo1234 <izumilitee@gmail.com>
2025-05-24 23:33:56 -0600

    Update _xd.js

--- plugins/_xd.js
+++ plugins/_xd.js
@@ -1,29 +1,33 @@
-import { sticker } from '../lib/sticker.js';
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)];
+  return list[Math.floor(Math.random() * list.length)]
 }
 
-let handler = async (m, { conn }) => {
+const handler = async (m, { conn }) => {
   try {
-    let stikerxd = [
+    const stikerxd = [
       'https://telegra.ph/file/e8be85aeb9a625f533a4a.png',
       'https://telegra.ph/file/913f5861cefbdde379921.jpg',
       'https://telegra.ph/file/6b7b0dbf022ee46a44887.jpg'
-    ];
-    let stikerUrl = pickRandom(stikerxd); 
-    let stiker = await sticker(null, stikerUrl, global.packname, global.author);
-    if (stiker) {
-      await conn.sendMessage(m.chat, { sticker: { url: stiker } }, { quoted: m });
-    } else {
-      throw new Error('Failed to create sticker'); 
-    }
-  } catch {
-    // Manejo de error opcional
+    ]
+    
+    const url = pickRandom(stikerxd)
+    const imgBuffer = await fetch(url).then(res => res.buffer())
+    const webpBuffer = await sticker(imgBuffer, false, global.packname, global.author)
+
+    await conn.sendMessage(
+      m.chat,
+      { sticker: webpBuffer },
+      { quoted: m }
+    )
+  } catch (e) {
+    console.error('Error al enviar sticker xd:', e)
   }
-};
+}
 
 handler.customPrefix = /xd/
-handler.command = new RegExp;
+handler.command = new RegExp
 
-export default handler;
+export default handler
