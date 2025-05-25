dfc34cc97283ff058ea1d0cca7996d73c3e50ebd
gudo1234 <izumilitee@gmail.com>
2025-05-24 18:46:59 -0600

    Update _edi.js

--- plugins/_edi.js
+++ plugins/_edi.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 import PhoneNum from 'awesome-phonenumber';
 import moment from 'moment-timezone';
 import 'moment/locale/es.js';
@@ -47,6 +47,90 @@ async function handler(m, { conn }) {
 *País:* ${countryName} ${emojiBandera}
 *Capital:* ${capital}
 *Fecha:* ${fechaLocal}
+  `.trim();
+
+  m.reply(info);
+}
+
+handler.command = ['edi'];
+export default handler;*/
+
+import axios from 'axios';
+import PhoneNum from 'awesome-phonenumber';
+import moment from 'moment-timezone';
+import 'moment/locale/es.js';
+
+moment.locale('es');
+
+const regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
+
+function banderaEmoji(countryCode) {
+  if (!countryCode || countryCode.length !== 2) return '';
+  const codePoints = [...countryCode.toUpperCase()]
+    .map(char => 0x1F1E6 + char.charCodeAt(0) - 65);
+  return String.fromCodePoint(...codePoints);
+}
+
+async function handler(m, { conn }) {
+  const jid = m.sender;
+  const number = jid.split('@')[0];
+  const name = await conn.getName(jid);
+
+  const phoneInfo = PhoneNum('+' + number);
+  const countryCode = phoneInfo.getRegionCode('international');
+  const countryName = regionNames.of(countryCode) || 'Desconocido';
+  const emojiBandera = banderaEmoji(countryCode);
+
+  let capital = 'Desconocida';
+  let fechaLocal = 'No disponible';
+
+  try {
+    const res = await axios.get(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+    const data = res.data[0];
+    capital = data.capital?.[0] || 'Desconocida';
+
+    const zonaHoraria = data.timezones?.[0];
+    if (zonaHoraria) {
+      const now = moment().tz(zonaHoraria);
+      fechaLocal = now.format('dddd, D [de] MMMM [de] YYYY');
+    }
+  } catch (e) {
+    console.error('Error al obtener datos del país:', e);
+  }
+
+  // Intentar obtener perfil business
+  let businessInfo = '';
+  try {
+    // La función puede cambiar según la versión de tu librería
+    // prueba conn.fetchBusinessProfile o conn.getBusinessProfile
+    const businessProfile = await conn.fetchBusinessProfile(jid).catch(() => null);
+    
+    if (businessProfile && businessProfile.businessProfile) {
+      const bp = businessProfile.businessProfile;
+      const desc = bp.description || 'No disponible';
+      const email = bp.email || 'No disponible';
+      const website = bp.website || 'No disponible';
+      const address = bp.address || 'No disponible';
+
+      businessInfo = `
+*WhatsApp Business*:
+- Descripción: ${desc}
+- Email: ${email}
+- Web: ${website}
+- Dirección: ${address}
+      `.trim();
+    }
+  } catch (e) {
+    // No es business o no hay info
+  }
+
+  const info = `
+*Nombre:* ${name}
+*Número:* +${number}
+*País:* ${countryName} ${emojiBandera}
+*Capital:* ${capital}
+*Fecha:* ${fechaLocal}
+${businessInfo ? '\n' + businessInfo : ''}
   `.trim();
 
   m.reply(info);
