782135797702556047675cdcc859b9f2824c8ecc
gudo1234 <izumilitee@gmail.com>
2025-05-26 13:42:47 -0600

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -1,28 +1,16 @@
-let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
-    let a = []
-    let b = text.split('|')
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-    if (!b[0]) return conn.reply(m.chat, `${e} *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
-    if (!b[1]) return conn.reply(m.chat, `${e} _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
-    if (b.length > 13) return conn.reply(m.chat, `${e} Lo máximo que debes poner es un total de *13* opciones.`, m)
-
-    for (let c = 1; c < b.length; c++) { 
-        a.push([b[c]]) 
-    }
-    
-    let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
-
-    let mentions = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    const poll = {
-        name: [b[0]],
-        selectableCount: 1,
-        values: a,
-        mentions: mentions,
-        remoteJid: m.chat
-    }  
-
-    return await conn.sendPoll(m.chat, texto, a, { poll })
+if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+let a = []
+let b = text.split('|')
+for (let c = 0; c < b.length; c++) {
+a.push([b[c]])
+                        }
+                        return conn.sendPoll(m.chat, `${wm}`, a, m)
 }
 
-handler.command = ['poll', 'encuesta', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
+handler.command = ['poll', 'encuesta'] 
+handler.group = true
+
 export default handler
