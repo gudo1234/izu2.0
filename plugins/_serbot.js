c7308bf1c702011dbf229cb95ffa19dbac94fea8
gudo1234 <izumilitee@gmail.com>
2025-05-25 22:14:29 -0600

    Delete plugins/_serbot.js

--- plugins/_serbot.js
+++
@@ -1,8 +0,0 @@
-/*let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  conn.reply(m.chat, `${e} Comando deshabilitado por el desarrollador.`, m)
-}
-
-handler.command = ['qr', 'code']
-handler.group = true
-export default handler*/
