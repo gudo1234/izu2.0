41d58fb78de62fded7c0e536b3438711151a2ea0
gudo1234 <izumilitee@gmail.com>
2025-05-26 22:41:57 -0600

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -32,7 +32,7 @@ const handler = async (m, { text, conn, args }) => {
   let video = data.url;
   try {
     //await conn.sendMessage(m.chat, { video: { url: video }, caption: `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
-  await conn.sendFile(m.chat, video, `thumbnail.mp4`, `> ${textbot}`, m, null, rcanal)
+  await conn.sendFile(m.chat, video, `thumbnail.mp4`, `${e} _Video de facebook_`, m, null, rcanal)
   
     await m.react(done);
   } catch (e) {
