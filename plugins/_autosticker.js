2ad4a6a3fb1073f12143e7e93f0f4ab76f88b28f
gudo1234 <izumilitee@gmail.com>
2025-05-31 11:57:34 -0600

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -18,14 +18,14 @@ stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
 let img = await q.download()
 if (!img) return
-stiker = await sticker(img, false, packname, author)
+stiker = await sticker(img, false, `${m.pushName}`)
 } else if (m.text.split(/\n| /i)[0]) {
 if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author)
 else return
 }
 if (stiker) {
 //await this.sendFile(m.chat, stiker, null, { asSticker: true })
-await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
+await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: null })
 }}
 return !0
 
