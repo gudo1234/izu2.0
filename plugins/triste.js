5744a825be88af7f6b813b4cd64f64c8c9bb5ecc
gudo1234 <izumilitee@gmail.com>
2025-05-30 18:29:18 -0600

    Update triste.js

--- plugins/triste.js
+++ plugins/triste.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   let vn = './media/prueba.mp3' //canwe kis forever
-  let vn2 = './media/prueba2.mp3'
+  let vn2 = './media/prueba2.mp3' //death bed
   let vn3 = './media/snow.mp3'
   let vn4 = './media/sad.mp3'
   let vn5 = './media/cardigansad.mp3'
