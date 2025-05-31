cfc2ed02aa2e9bb8fbcb9a8dbc2dd290fe7c3a82
gudo1234 <izumilitee@gmail.com>
2025-05-31 11:52:29 -0600

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -2,45 +2,35 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 
-handler.all = async function (m, { conn }) {
-  let chat = db.data.chats[m.chat]
-  if (!chat.autosticker || !m.isGroup) return
+handler.all = async function (m) {
+let chat = db.data.chats[m.chat]
+let user = db.data.users[m.sender]
 
-  let mime = (m.msg || m).mimetype || m.mediaType || ''
-  let stiker = false
-  const name = m.pushName || 'Sticker'
-
-  try {
-    if (/webp/g.test(mime)) return
-
-    if (/image/g.test(mime)) {
-      const img = await m.download?.()
-      if (!img) return
-      stiker = await sticker(img, false, name, name)
-    } else if (/video/g.test(mime)) {
-      const video = await m.download()
-      if (!video) return
-      stiker = await sticker(video, false, name, name)
-    } else if (isUrl(m.text)) {
-      const url = m.text.split(/\n| /i)[0]
-      stiker = await sticker(false, url, name, name)
-    }
-
-    if (stiker) {
-      await conn.sendMessage(
-        m.chat,
-        { sticker: stiker },
-        { quoted: m }
-      )
-    }
-  } catch (e) {
-    console.error('Error al generar autosticker:', e)
-  }
-
-  return !0
+if (chat.autosticker && m.isGroup) {
+let q = m
+let stiker = false
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/webp/g.test(mime)) return
+if (/image/g.test(mime)) {
+let img = await q.download?.()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (/video/g.test(mime)) {
+let img = await q.download()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (m.text.split(/\n| /i)[0]) {
+if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author)
+else return
 }
+if (stiker) {
+//await this.sendFile(m.chat, stiker, null, { asSticker: true })
+await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: null })
+}}
+return !0
 
+}
 export default handler
 
-const isUrl = (text = '') =>
-  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/gi.test(text)
+const isUrl = (text) => {
+return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
