bfe5bb755a4547af396b720d2c7d9ab52617fe80
gudo1234 <izumilitee@gmail.com>
2025-05-31 08:50:28 -0600

    Update country.js

--- plugins/country.js
+++ plugins/country.js
@@ -1,6 +1,5 @@
 import moment from 'moment-timezone';
 let userMessageCount = {};
-if (chat.autoband && m.isGroup) {
 let flags = [
   {
     "name": "Afganistán",
@@ -2411,5 +2410,4 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         m.react('✖️');
         await conn.reply(m.chat, `*¡Respuesta Incorrecta!*\n> vuelve a intentar\n🧩 _*Pista:* Su código de área es *${userMessageCount[m.chat].currentFlag3}* ${userMessageCount[m.chat].currentFlag2}_ \n⏳ *Tiempo restante:* _${minutesRemaining} minutos y ${secondsRemaining} segundos._`, m);
     }
-}
 }
