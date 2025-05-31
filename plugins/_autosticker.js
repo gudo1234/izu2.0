cb1acdb4e6f1c50feb2ec756aa2d5835e16989f1
gudo1234 <izumilitee@gmail.com>
2025-05-31 11:49:26 -0600

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -2,37 +2,39 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 
-handler.all = async function (m) {
+handler.all = async function (m, { conn }) {
   let chat = db.data.chats[m.chat]
+  if (!chat.autosticker || !m.isGroup) return
 
-  if (chat.autosticker && m.isGroup) {
-    let q = m
-    let stiker = false
-    let mime = (q.msg || q).mimetype || q.mediaType || ''
+  let mime = (m.msg || m).mimetype || m.mediaType || ''
+  let stiker = false
+  const name = m.pushName || 'Sticker'
 
-    let username = m.pushName || 'Sticker'
-
+  try {
     if (/webp/g.test(mime)) return
 
     if (/image/g.test(mime)) {
-      let img = await q.download?.()
+      const img = await m.download?.()
       if (!img) return
-      stiker = await sticker(img, false, username, '')
+      stiker = await sticker(img, false, name, name)
     } else if (/video/g.test(mime)) {
-      let img = await q.download()
-      if (!img) return
-      stiker = await sticker(img, false, username, '')
-    } else if (m.text.split(/\n| /i)[0]) {
-      if (isUrl(m.text)) {
-        stiker = await sticker(false, m.text.split(/\n| /i)[0], username, '')
-      } else return
+      const video = await m.download()
+      if (!video) return
+      stiker = await sticker(video, false, name, name)
+    } else if (isUrl(m.text)) {
+      const url = m.text.split(/\n| /i)[0]
+      stiker = await sticker(false, url, name, name)
     }
 
     if (stiker) {
-      await this.sendMessage(m.chat, {
-        sticker: { url: stiker }
-      }, { quoted: m })
+      await conn.sendMessage(
+        m.chat,
+        { sticker: stiker },
+        { quoted: m }
+      )
     }
+  } catch (e) {
+    console.error('Error al generar autosticker:', e)
   }
 
   return !0
@@ -40,6 +42,5 @@ handler.all = async function (m) {
 
 export default handler
 
-const isUrl = (text) => {
-  return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))
-}
+const isUrl = (text = '') =>
+  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/gi.test(text)
