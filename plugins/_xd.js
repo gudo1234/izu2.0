f5fee3ffe8c0fc88c24678a58d0ce391385f5b8d
gudo1234 <izumilitee@gmail.com>
2025-05-31 22:41:53 -0600

    Update _xd.js

--- plugins/_xd.js
+++ plugins/_xd.js
@@ -14,7 +14,7 @@ const handler = async (m, { conn }) => {
     ]
     
     const url = pickRandom(stikerxd)
-    const imgBuffer = await fetch(url).then(res => res.buffer())
+    const imgBuffer = await fetch(icono).then(res => res.buffer())
     const webpBuffer = await sticker(imgBuffer, false, `hola`)
 
     await conn.sendMessage(
