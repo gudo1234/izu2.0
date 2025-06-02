6262fb834146583470bf8c0b6c2e45a176adf023
gudo1234 <izumilitee@gmail.com>
2025-06-02 14:55:56 -0600

    Update _ping.js

--- plugins/_ping.js
+++ plugins/_ping.js
@@ -1,19 +1,14 @@
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     const start = Date.now()
 
-    await conn.sendMessage(m.chat, {
-        text: 'Pong!',
-        quoted: m
-    })
-
     const end = Date.now()
     const latency = end - start
-
     await conn.sendMessage(m.chat, {
         text: `Tiempo de respuesta: ${latency}ms`,
         quoted: m
     })
+    m.rep
 }
 
-handler.command = ['ping', '0']
+handler.command = ['ping', 'p']
 export default handler
