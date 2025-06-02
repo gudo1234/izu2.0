efedd4668cd82c0f635e98c755b083af14d83c8e
gudo1234 <izumilitee@gmail.com>
2025-06-02 13:34:16 -0600

    Update reglas.js

--- plugins/reglas.js
+++ plugins/reglas.js
@@ -1,9 +1,17 @@
-import { getDevice } from "@whiskeysockets/baileys"
-import PhoneNumber from 'awesome-phonenumber'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-const thumbnail = await (await fetch(icono)).buffer()
-m.react('🍉')
-let txt = `${e} _*Hola ${m.pushName}*._
+import moment from 'moment-timezone'
+import 'moment/locale/es'  // importa el locale español
+
+let handler = async (m, { conn }) => {
+  const thumbnail = await (await fetch(icono)).buffer()
+  m.react('🍉')
+
+  // Establece el locale a español
+  moment.locale('es')
+
+  // Obtiene la fecha completa en español
+  let fechaEsp = moment.tz('America/Bogota').format('dddd, D [de] MMMM [de] YYYY')
+
+  let txt = `${e} _*Hola ${m.pushName}*._
 
 ⚖️ \`Términos y Condiciones del Servicio\`
 
@@ -13,33 +21,34 @@ let txt = `${e} _*Hola ${m.pushName}*._
 
 🤖 *Este servicio se proporciona “tal cual”, sin garantías explícitas o implícitas. Izubot se reserva el derecho de modificar o interrumpir el servicio en cualquier momento sin previo aviso.*
 
-🗓 *Fecha:* ${moment.tz('america/bogota').format('dd/mm/yyyy')}
+🗓 *Fecha:* ${fechaEsp}
 
 > © ${new Date().getFullYear()} Izubot. Todos los derechos reservados.`
-await conn.sendMessage(m.chat, {
-      text: txt,
-      footer: textbot,
-      contextInfo: {
-          mentionedJid: [m.sender],
-          isForwarded: true,
-          forwardedNewsletterMessageInfo: {
-              newsletterJid: channelRD.id,
-              newsletterName: channelRD.name,
-              serverMessageId: -1,
-          },
-          forwardingScore: false,
-          externalAdReply: {
-              title: botname,
-              body: textbot,
-              thumbnailUrl: redes,
-              thumbnail,
-            sourceUrl: redes,
-              mediaType: 1,
-              showAdAttribution: true,
-              renderLargerThumbnail: true,
-          },
+
+  await conn.sendMessage(m.chat, {
+    text: txt,
+    footer: textbot,
+    contextInfo: {
+      mentionedJid: [m.sender],
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        newsletterName: channelRD.name,
+        serverMessageId: -1,
       },
+      forwardingScore: false,
+      externalAdReply: {
+        title: botname,
+        body: textbot,
+        thumbnailUrl: redes,
+        thumbnail,
+        sourceUrl: redes,
+        mediaType: 1,
+        showAdAttribution: true,
+        renderLargerThumbnail: true,
+      },
+    },
   }, { quoted: m });
-    }
+}
 handler.command = ['reglas', 'términos', 'condiciones']
 export default handler
