09b18e09d365af2ee95fc36e34169e6511c89a85
gudo1234 <izumilitee@gmail.com>
2025-06-03 15:26:59 -0600

    Update iqc.js

--- plugins/iqc.js
+++ plugins/iqc.js
@@ -3,20 +3,11 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, text }) => {
   if (!text) return m.reply(`${e} Por favor ingresa un texto!*\nEjemplo: .iqc Hola Mundo`);
   if (text.length > 80) return m.reply(`${e} El texto es demasiado largo!*\nMáximo 80 caracteres.`);
-m.reply('🕒')
+m.react('🕒')
   try {
     const apiUrl = `https://flowfalcon.dpdns.org/imagecreator/iqc?text=${encodeURIComponent(text)}`;
-
-    await conn.sendMessage(m.chat, {
-      image: { url: apiUrl },
-      caption: `🍎 *iPhone* = ${text}`
-    }, { quoted: m });
+    await conn.sendFile(m.chat, apiUrl, "Thumbnail.jpg", `🍎 *iPhone* = ${text}`, m, null, rcanal)
 m.react('✅')
-    await conn.sendMessage(m.chat, {
-      text: `🍎 *iPhone* = ${text}`,
-      edit: mensajeCargando.key
-    });
-
   } catch (error) {
     console.error('Error:', error);
     await conn.sendMessage(m.chat, {
