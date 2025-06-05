1bb8a71345e20bd956dc1f6d99325d3fdcfe0d4d
gudo1234 <izumilitee@gmail.com>
2025-06-05 16:30:29 -0600

    Update _json.js

--- plugins/_json.js
+++ plugins/_json.js
@@ -1,8 +1,14 @@
 import fs from 'fs';
+
 const disabledCmdFile = './comandos-desactivados.json';
 let disabledCmds = [];
 if (fs.existsSync(disabledCmdFile)) {
-  disabledCmds = JSON.parse(fs.readFileSync(disabledCmdFile));
+  try {
+    disabledCmds = JSON.parse(fs.readFileSync(disabledCmdFile));
+  } catch (e) {
+    console.error('Error al leer comandos desactivados:', e);
+    disabledCmds = [];
+  }
 }
 if (disabledCmds.includes(command)) {
   return conn.reply(m.chat, `❌ El comando *${command}* está desactivado. Usa *.activar ${command}* para reactivarlo.`, m);
