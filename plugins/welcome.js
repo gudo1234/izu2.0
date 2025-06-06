3943988c4c8aecdb0313400dfd562cc5e4005a5d
gudo1234 <izumilitee@gmail.com>
2025-06-06 14:02:17 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -6,8 +6,8 @@ export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
   //😍mi desmadre
   let who = m.messageStubParameters[0] + '@s.whatsapp.net'
-  let user = global.db.data.users[who]
-  let tag = user ? user.name : await conn.getName(who)
+let user = global.db.data.users[who]
+let tag = (user && user.name) || await conn.getName(who) || '@' + who.split('@')[0]
   
   let vn = './media/a.mp3'; //welcome bendicion
   let vn2 = './media/bien.mp3'; //welcome entra épica
