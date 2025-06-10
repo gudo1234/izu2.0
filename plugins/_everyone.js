42ae273aab25992071af5d1b68c5c2baf58f7a71
gudo1234 <izumilitee@gmail.com>
2025-06-10 07:29:41 -0600

    Create _everyone.js

---
+++ plugins/_everyone.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, participants, text, groupMetadata, isAdmin, isOwner, isBotAdmin }) => {
+  if (!m.isGroup) throw 'Este comando solo funciona en grupos';
+  if (!isAdmin && !isOwner) throw 'Solo los admins pueden usar este comando';
+  //if (!isBotAdmin) throw 'Necesito ser admin para mencionar a todos';
+
+  const groupName = groupMetadata.subject;
+  const users = participants.map(p => p.id).filter(id => id !== conn.user.jid);
+  const msg = text || 'Hola a todos 👋';
+
+  await conn.sendMessage(m.chat, {
+    text: `@*${groupName}* ${msg}`,
+    mentions: users
+  }, { quoted: m });
+};
+
+handler.command = ['everyone']
+handler.group = true;
+handler.admin = true;
+export default handler;
