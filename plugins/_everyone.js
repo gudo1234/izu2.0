0c81eab7bf23a59255c9f8e604dbd8c03a96cef6
gudo1234 <izumilitee@gmail.com>
2025-06-10 10:47:53 -0600

    Create _everyone.js

---
+++ plugins/_everyone.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, participants, text }) => {
+  const metadata = await conn.groupMetadata(m.chat);
+  const members = metadata.participants.map(p => p.id);
+  //const whitelist = global.db.data.chats[m.chat]?.whitelist || []; // Asumiendo whitelist desde base de datos
+
+  const usuarios = members.filter(id => !whitelist.includes(id));
+  const message = text?.trim() || 'Hola 😃';
+
+  await m.reply(`@${m.sender} ${message}`, {
+    contextInfo: {
+      mentionedJid: usuarios,
+      groupMentions: [{
+        groupJid: m.chat,
+        groupSubject: metadata.subject
+      }]
+    }
+  });
+};
+
+handler.command = ['everyone'];
+handler.group = true;
+handler.admin = true;
+export default handler;
