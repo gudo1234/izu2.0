b861ae871f3ebc38cda9871bb37d9e5f3181fbff
gudo1234 <izumilitee@gmail.com>
2025-06-10 10:50:45 -0600

    Update _everyone.js

--- plugins/_everyone.js
+++ plugins/_everyone.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, participants, text }) => {
   const members = metadata.participants.map(p => p.id);
   //const whitelist = global.db.data.chats[m.chat]?.whitelist || []; // Asumiendo whitelist desde base de datos
 
-  const usuarios = members.filter(id => !whitelist.includes(id));
+  //const usuarios = members.filter(id => !whitelist.includes(id));
   const message = text?.trim() || 'Hola 😃';
 
   await m.reply(`@${m.sender} ${message}`, {
