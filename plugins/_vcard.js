94ef493535b487e9150a800bfb59a7c0621b0d21
gudo1234 <izumilitee@gmail.com>
2025-06-11 19:20:05 -0600

    Update _vcard.js

--- plugins/_vcard.js
+++ plugins/_vcard.js
@@ -2,33 +2,51 @@ import axios from 'axios';
 import PhoneNum from 'awesome-phonenumber';
 
 const regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
+
 function banderaEmoji(countryCode) {
   if (!countryCode || countryCode.length !== 2) return '';
   const codePoints = [...countryCode.toUpperCase()]
     .map(char => 0x1F1E6 + char.charCodeAt(0) - 65);
   return String.fromCodePoint(...codePoints);
 }
+
 async function handler(m, { conn, text, mentionedJid }) {
   let number, jid;
+
+  // 1. Si se menciona a alguien con @
   if (mentionedJid?.length > 0) {
     jid = mentionedJid[0];
     number = jid.split('@')[0];
   }
+
+  // 2. Si se responde a un mensaje de alguien
+  else if (m.quoted?.sender) {
+    jid = m.quoted.sender;
+    number = jid.split('@')[0];
+  }
+
+  // 3. Si se escribe manualmente un número
   else if (/^\+?\d{5,16}$/.test(text.trim())) {
     number = text.replace(/\D/g, '');
     jid = number + '@s.whatsapp.net';
   }
+
+  // 4. Si no se pone nada, usar al autor del mensaje
   else {
     jid = m.sender;
     number = jid.split('@')[0];
   }
+
   const name = await conn.getName(jid);
   const { data: thumbnail } = await axios.get(icono, { responseType: 'arraybuffer' });
+
   const phoneInfo = PhoneNum('+' + number);
   const countryCode = phoneInfo.getRegionCode('international');
   const countryName = regionNames.of(countryCode) || 'Desconocido';
   const emojiBandera = banderaEmoji(countryCode);
+
   const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
+
   await conn.sendMessage(m.chat, {
     contacts: {
       contacts: [{
@@ -51,5 +69,5 @@ async function handler(m, { conn, text, mentionedJid }) {
 }
 
 handler.command = ['vcard']
-handler.group = true;
+
 export default handler;
