3489b5852fd0dd8bd479e865b81e7b8c4890b549
gudo1234 <izumilitee@gmail.com>
2025-06-13 07:05:40 -0600

    Update _fetch.js

--- plugins/_fetch.js
+++ plugins/_fetch.js
@@ -1,50 +1,35 @@
 import fetch from 'node-fetch'
-import { format } from 'util'
 
 let handler = async (m, { text, conn }) => {
-if (!/^https?:\/\//.test(text)) return conn.reply(m.chat, 'Ejemplo:\nhttps://pornhub.com', m)
-let _url = new URL(text)
-let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
-let res = await fetch(url)
-if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-return m.reply(`Content-Length: ${res.headers.get('content-length')}`)
-}
-if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
-let txt = await res.buffer()
-try {
-txt = format(JSON.parse(txt + ''))
-} catch (e) {
-txt = txt + ''
-} finally {
-m.reply(txt.slice(0, 65536) + '')
-}}
+  if (!/^https?:\/\//.test(text)) return m.reply('Ejemplo:\n.fetch https://giffiles.alphacoders.com/221/thumb-440-221903.mp4')
 
-handler.help = ['fetch'].map(v => v + ' *( Link )*')
+  try {
+    let res = await fetch(text)
+
+    let size = res.headers.get('content-length') || 0
+    if (size > 100 * 1024 * 1024) return m.reply(`El archivo es demasiado grande: ${(size / (1024 * 1024)).toFixed(2)} MB`)
+
+    let type = res.headers.get('content-type') || ''
+    if (!/text|json/.test(type)) {
+      return conn.sendFile(m.chat, text, 'archivo', `📥 Archivo desde: ${text}`, m)
+    }
+
+    // Si es texto o JSON, lo intentamos mostrar como texto plano
+    let txt = await res.buffer()
+    try {
+      txt = JSON.stringify(JSON.parse(txt.toString()), null, 2)
+    } catch (e) {
+      txt = txt.toString()
+    }
+    m.reply(txt.slice(0, 65536)) // máximo 65k caracteres
+  } catch (err) {
+    m.reply('❌ Error al obtener el archivo:\n' + err)
+  }
+}
+
+handler.help = ['fetch'].map(v => v + ' <link>')
 handler.tags = ['owner']
 handler.command = ['fetch', 'get']
-handler.rowner = true 
+handler.rowner = true
 
 export default handler
-
-global.APIs = {}
-global.APIKeys = {}
-    
-global.API = (name, path = "/", query = {}, apikeyqueryname) =>
-(name in global.APIs ? global.APIs[name] : name) +
-path +
-(query || apikeyqueryname
-? "?" +
-new URLSearchParams(
-Object.entries({
-...query,
-...(apikeyqueryname
-? {
-[apikeyqueryname]:
-global.APIKeys[
-name in global.APIs ? global.APIs[name] : name
-],
-}
-: {}),
-}),
-)
-: "");
