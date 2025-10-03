8f94e0e098a8680da6bae66eada7f8674a53b2a0
gudo1234 <izumilitee@gmail.com>
2025-10-03 17:28:36 -0600

    Update xnxxporno.js

--- plugins/xnxxporno.js
+++ plugins/xnxxporno.js
@@ -51,7 +51,7 @@ const handler = async (m, { text, usedPrefix, command }) => {
     await conn.sendFile(m.chat, dl_url, title + '.mp4', `╭───── • ─────╮
   𖤐 \`XNXX EXTRACTOR\` 𖤐
 ╰───── • ─────╯
-✦ *Título:* ${title}`, m);
+✦ *Título:* ${title}`, m, null, rcanal);
     global.videoListXXX.push(vids_);
   } catch (err) {
     return conn.reply(m.chat, `${e} Ocurrió un error: ${err.message}`, m);
