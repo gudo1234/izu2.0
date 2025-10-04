34382a9ee9984f96e2485b874e51d730b514cfcc
gudo1234 <izumilitee@gmail.com>
2025-10-04 11:38:18 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -247,7 +247,7 @@ let _user = global.db.data && global.db.data.users && global.db.data.users[m.sen
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
-const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {} // User Data
+const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id && u.jid) === m.sender) : {}) || {}
 const numBott = (this.user.lid || '').replace(/:.*/, '') || false
 const detectnumbot = m.sender.includes('@lid') ? `${numBott}@lid` : this.user.jid
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectnumbot) : {}) || {}
