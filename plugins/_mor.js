0b2e5b7ac5df1c5e1cd29f33f6ff66e6fd051cff
gudo1234 <izumilitee@gmail.com>
2025-10-04 00:15:24 -0600

    Update _mor.js

--- plugins/_mor.js
+++ plugins/_mor.js
@@ -2,19 +2,17 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
   // Audios disponibles
-  const audios = [
-    './media/prueba.mp3',
-    './media/prueba2.mp3',
-    './media/snow.mp3',
-    './media/sad.mp3',
-    './media/cardigansad.mp3',
-    './media/iwas.mp3',
-    './media/juntos.mp3',
-    './media/space.mp3',
-    './media/stellar.mp3',
-    './media/theb.mp3',
-    './media/alanspectre.mp3'
-  ]
+  let vn = './media/prueba.mp3' //canwe kis forever
+  let vn2 = './media/prueba2.mp3' //death bed
+  let vn3 = './media/snow.mp3' //snow fall
+  let vn4 = './media/sad.mp3' //sad tortuga 
+  let vn5 = './media/cardigansad.mp3' // sad luka
+  let vn6 = './media/iwas.mp3'
+  let vn7 = './media/juntos.mp3'
+  let vn8 = './media/space.mp3'
+  let vn9 = './media/stellar.mp3'
+  let vn10 = './media/theb.mp3'
+  let vn11 = './media/alanspectre.mp3'
   // Intentar obtener la foto de perfil
   let pp
   try {
@@ -29,7 +27,7 @@ let handler = async (m, { conn }) => {
   await conn.sendFile(
     m.chat,
     {
-      audio: { url: audios[Math.floor(Math.random() * audios.length)] },
+      audio: [vn, vn2, vn4, vn5, vn7].getRandom(),
       ptt: true,
       mimetype: 'audio/mpeg',
       fileName: 'welcome.mp3',
