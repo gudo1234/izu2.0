34bf53a46db124b6c4afa5806c09d7e4406f8832
gudo1234 <izumilitee@gmail.com>
2025-10-11 15:41:10 -0600

    Update _puta.js

--- plugins/_puta.js
+++ plugins/_puta.js
@@ -6,7 +6,6 @@ async function sendPTT(conn, m, filePath = './media/puta.mp3') {
 
   // convierte el mp3 (o cualquier formato) a OGG Opus compatible con WhatsApp
   execSync(`ffmpeg -y -i "${filePath}" -vn -c:a libopus -b:a 64k -vbr on -application voip "${outputPath}"`);
-
   // envía como nota de voz (PTT)
   await conn.sendMessage(m.chat, {
     audio: fs.readFileSync(outputPath),
