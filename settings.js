4b90b82fe9cfa116f3a7c73101c7e313fd7dc11a
gudo1234 <izumilitee@gmail.com>
2025-10-12 13:10:53 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.namebot = '🤖⃧►iʑυвöτ◃2.0▹'
 global.sessions = 'Sessions'
 global.jadi = 'JadiBots' 
 global.yukiJadibts = true
-global.apiKey = '2yLJjTeqXudWiWB8'
+global.apiKey = 'stellar-8CQatPnA'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
