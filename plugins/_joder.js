ee3db733360b6acfe66aebfa31c93abe4fe0b45f
gudo1234 <izumilitee@gmail.com>
2025-10-15 17:29:51 -0600

    Update video URL and caption in _joder.js

--- plugins/_joder.js
+++ plugins/_joder.js
@@ -2,11 +2,13 @@ const handler = async (m, { conn, participants }) => {
   const users = participants
     .map(u => u.id)
     .filter(id => id !== conn.user.jid);
-
+let vid = 'https://files.catbox.moe/xdijl8.mp4' // México 
+//let vid = 'https://files.catbox.moe/f5e14a.mp4' // Mia
+let caption = `Ayuda el Narco✅ = 🇲🇽 *Seré un villano no un monstruo*\n\nAyuda los influencers✅\nAyuda el Gobierno❌`
   try {
     await conn.sendMessage(m.chat, {
-      video: { url: 'https://files.catbox.moe/f5e14a.mp4' },
-      caption: '🤨 cómo así lidel',
+      video: { url: vid },
+      caption,
       mentions: users
     }, { quoted: null });
   } catch (err) {
