43a993ed45cfc69b1cdfdf25fe43a03884dd7051
gudo1234 <izumilitee@gmail.com>
2025-10-17 15:53:21 -0600

    Update _ni.js

--- plugins/_ni.js
+++ plugins/_ni.js
@@ -1,27 +1,19 @@
 import fs from 'fs'
 
 let handler = async (m, { conn }) => {
-  let imgBot = 'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me6.jpg' // URL de imagen completa
-  let txt = `
-Hola! Soy #K1NG_◇ <3
-Bienvenido/a al menú de #K1NG_◇
-
-╭─❖ *CANAL* ❖─
-https://whatsapp.com/channel/0029Vb6OdX884Om9sylf6j1e
-╰─────────────
-`
-
+m.react('✅')
   await conn.sendMessage(m.chat, {
-    image: { url: imgBot },
+    image: { url: icono },
     caption: txt.trim(),
     contextInfo: {
       externalAdReply: {
         title: "hola",
         body: "Mi bot xd",
-        thumbnailUrl: "https://whatsapp.com/channel/0029Vb6OdX884Om9sylf6j1e",
-        sourceUrl: "https://whatsapp.com/channel/0029Vb6OdX884Om9sylf6j1e",
+        thumbnailUrl: redes,
+        thumbnail: icono,
+        sourceUrl: redes,
         mediaType: 1,
-        showAdAttribution: true,
+        showAdAttribution: false,
         renderLargerThumbnail: true
       }
     }
