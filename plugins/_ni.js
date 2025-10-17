838014c13c1a00c12a0725c22feab071611496f6
gudo1234 <izumilitee@gmail.com>
2025-10-17 16:06:26 -0600

    Update _ni.js

--- plugins/_ni.js
+++ plugins/_ni.js
@@ -4,18 +4,16 @@ let handler = async (m, { conn }) => {
   m.react('✅')
 
   await conn.sendMessage(m.chat, {
-    image: { url: icono }, // tu imagen principal
-    caption: 'no',
-    contextInfo: {
-      externalAdReply: {
-        title: "hola",
-        body: "Mi bot xd",
-        mediaUrl: redes,          // 🔥 Enlaza aquí el link del canal o red
-        sourceUrl: redes,         // igual que arriba, por compatibilidad
-        mediaType: 1,
-        renderLargerThumbnail: true, // 🔥 Hace que la imagen se vea completa
-        showAdAttribution: false,
-        thumbnailUrl: icono        // 🔥 usa solo thumbnailUrl, NO uses 'thumbnail'
+  text: 'xdd',
+  contextInfo: {
+  externalAdReply: {
+  title: "hola",
+  body: "Mi bot xd",
+  sourceUrl: redes,
+  mediaType: 1,
+  renderLargerThumbnail: true,
+  thumbnail:  icono,
+  thumbnailUrl: redes
       }
     }
   }, { quoted: m })
