ad577acb0314214ccc4f70d25eb16b9e50bd767b
gudo1234 <izumilitee@gmail.com>
2025-10-17 16:10:03 -0600

    Update _ni.js

--- plugins/_ni.js
+++ plugins/_ni.js
@@ -4,19 +4,19 @@ let handler = async (m, { conn }) => {
   m.react('✅')
 
   await conn.sendMessage(m.chat, {
-  text: 'xdd',
-  contextInfo: {
-  externalAdReply: {
-  title: "hola",
-  body: "Mi bot xd",
-  sourceUrl: redes,
-  mediaType: 1,
-  renderLargerThumbnail: false,
-  thumbnail:  icono,
-  thumbnailUrl: redes
+      text: 'hsloo',
+      contextInfo: {
+        externalAdReply: {
+          title: wm,
+          body: textbot,
+          thumbnailUrl: redes,
+          thumbnail,
+          sourceUrl: redes,
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
       }
-    }
-  }, { quoted: m })
+    }, { quoted: m })
 }
 
 handler.command = ['ni']
