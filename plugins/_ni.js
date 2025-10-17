a90c2c3b5d4329213dcf4b50937443db8e7c85bd
gudo1234 <izumilitee@gmail.com>
2025-10-17 17:20:08 -0600

    Update _ni.js

--- plugins/_ni.js
+++ plugins/_ni.js
@@ -2,26 +2,23 @@ const handler = async (m, { conn }) => {
 await conn.sendMessage(m.chat, {
 video: { url: 'https://files.catbox.moe/nchm7h.mp4' },
 gifPlayback: true,
-caption: 'Puta',
+caption: 'Prueba de GIF',
 contextInfo: {
-mentionedJid: [m.sender],
 isForwarded: true,
 forwardedNewsletterMessageInfo: {
 newsletterJid: '120363417186717632@newsletter',
-newsletterName: 'Canal de mierda',
-serverMessageId: -1,
+newsletterName: 'Canal de prueba',
+serverMessageId: -1
 },
-forwardingScore: false,
 externalAdReply: {
 title: 'negro',
 body: 'holaa',
-thumbnailUrl: 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H',
-thumbnail: 'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me5.jpg',
+thumbnailUrl: 'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me5.jpg',
 sourceUrl: 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H',
 mediaType: 1,
-showAdAttribution: false,
-},
-},
+showAdAttribution: false
+}
+}
 }, { quoted: m })
 }
 
