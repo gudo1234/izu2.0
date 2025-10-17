75b6d4ff4591a09e7f71ea9ebb2bac5bcbaf389e
gudo1234 <izumilitee@gmail.com>
2025-10-17 16:17:12 -0600

    Update _ni.js

--- plugins/_ni.js
+++ plugins/_ni.js
@@ -1,21 +1,21 @@
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn }) => {
   m.react('✅')
-const thumbnail = await (await fetch(icono)).buffer()
+  const thumbnail = await (await fetch(icono)).buffer()
   await conn.sendMessage(m.chat, {
-      text: 'hsloo',
-      contextInfo: {
-        externalAdReply: {
-          title: wm,
-          body: textbot,
-          thumbnailUrl: redes,
-          thumbnail,
-          sourceUrl: redes,
-          mediaType: 1,
-          renderLargerThumbnail: true
-        }
+    text: 'hsloo',
+    contextInfo: {
+      externalAdReply: {
+        title: wm,
+        body: textbot,
+        thumbnail,
+        sourceUrl: redes,
+        mediaType: 1,
+        renderLargerThumbnail: true
       }
-    }, { quoted: m })
+    }
+  }, { quoted: m })
 }
 
 handler.command = ['ni']
