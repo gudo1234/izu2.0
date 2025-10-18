5fca1d4ad1d72526eccb200890bc617ed95cf375
gudo1234 <izumilitee@gmail.com>
2025-10-17 21:15:50 -0600

    Update twitter.js

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -5,8 +5,8 @@ const handler = async (m, { conn, text, usedPrefix, command, args }) => {
     if (!args || !args[0]) return conn.reply(m.chat, `${emoji} Te faltó el link de una imagen/video de twitter.`, m);
     if (enviando) return; 
     enviando = true;
-
     try {
+        m.react('🕒')
         const apiResponse = await axios.get(`https://delirius-apiofc.vercel.app/download/twitterdl?url=${args[0]}`);
         const res = apiResponse.data;
 
@@ -17,7 +17,7 @@ const handler = async (m, { conn, text, usedPrefix, command, args }) => {
         } else if (res?.type === 'image') {
             await conn.sendFile(m.chat, res.media[0].url, `image.jpg`, `${e} _Imagen de Twitter, "X"_`, m, null, rcanal);
         }
-
+        m.react('✅')
         enviando = false;
         return;
 
