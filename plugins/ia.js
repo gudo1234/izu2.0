02ec7f99f6e9e7f78b0c859c5b8c1e26fbf9fe72
gudo1234 <izumilitee@gmail.com>
2025-10-20 07:13:15 -0600

    Update ia.js

--- plugins/ia.js
+++ plugins/ia.js
@@ -1,28 +1,23 @@
 import fetch from 'node-fetch'
 
-const handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) return m.reply(`🧠 *Uso correcto:*\n> ${usedPrefix + command} ¿Qué es la inteligencia artificial?`, m, fake)
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return m.reply(`🧠 *Uso correcto:*\n> ${usedPrefix + command} ¿Qué es la inteligencia artificial?`)
 
   try {
     m.react('💭')
-
     const api = `https://api.dorratz.com/ai/gpt?prompt=${encodeURIComponent(text)}`
     const res = await fetch(api)
     const data = await res.text()
 
-    const respuesta = data.trim()
-
-    await m.reply(`🤖 *ChatGPT responde:*\n\n${respuesta}`, m, fake)
+    await m.reply(`${data.trim()}`)
     m.react('✅')
-
   } catch (e) {
     console.error(e)
     m.react('❌')
-    await m.reply('❌ Ocurrió un error al conectarse con la IA.', m, fake)
+    await m.reply('❌ Ocurrió un error al conectarse con la IA.')
   }
 }
 
 handler.command = ['chatgpt', 'ia', 'bot']
-handler.group = true
 
 export default handler
