7d4db4159f4a3f9fae83e4f7f0d28871fb87ef42
gudo1234 <izumilitee@gmail.com>
2025-10-20 13:40:48 -0600

    Update _xd.js

--- plugins/_xd.js
+++ plugins/_xd.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 
 function pickRandom(list) {
@@ -30,4 +30,29 @@ const handler = async (m, { conn }) => {
 //handler.customPrefix = /xd/
 //handler.command = new RegExp
 handler.command = ['xd']
+export default handler*/
+
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+const handler = async (m, { conn }) => {
+  const stikerxd = [
+    'https://telegra.ph/file/e8be85aeb9a625f533a4a.png',
+    'https://telegra.ph/file/913f5861cefbdde379921.jpg',
+    'https://telegra.ph/file/6b7b0dbf022ee46a44887.jpg'
+  ]
+  
+  const url = pickRandom(stikerxd)
+  const imgBuffer = await fetch(url).then(res => res.buffer())
+  const webpBuffer = await sticker(imgBuffer, false, `hola`)
+
+  await conn.sendMessage(m.chat, { sticker: webpBuffer }, { quoted: m })
+}
+
+handler.customPrefix = /xd|xdd|xdddd/
+handler.command = new RegExp()
 export default handler
