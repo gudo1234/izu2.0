c28fef513e5449f61aa00cfd74e53843bdd5bdec
gudo1234 <izumilitee@gmail.com>
2025-10-20 10:25:46 -0600

    Update _pin.js

--- plugins/_pin.js
+++ plugins/_pin.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text }) => {
     if (json.status && json.data && json.data.parts) {
       const respuesta = json.data.parts.map(p => p.text).join('\n');
       m.reply(
-`${e + respuesta}`
+`${e + \t + respuesta}`
       );
     } else {
       m.reply(`${e} No se pudo obtener respuesta de Gemini.`);
