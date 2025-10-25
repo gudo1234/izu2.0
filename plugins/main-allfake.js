f0e9e3d707b549309251a460924052f9b11ee730
gudo1234 <izumilitee@gmail.com>
2025-10-25 13:21:24 -0600

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -1,170 +1,117 @@
-import pkg from '@whiskeysockets/baileys'
-import fs from 'fs'
 import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone'
-const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
-
-var handler = m => m
-handler.all = async function (m) {
-
-global.getBuffer = async function getBuffer(url, options) {
-try {
-options ? options : {}
-var res = await axios({
-method: "get",
-url,
-headers: {
-'DNT': 1,
-'User-Agent': 'GoogleBot',
-'Upgrade-Insecure-Request': 1
-},
-...options,
-responseType: 'arraybuffer'
-})
-return res.data
-} catch (e) {
-console.log(`Error : ${e}`)
-}}
-  
-global.creador = 'Wa.me/50492280729'
-global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/50492280729'
-global.namechannel = '🤖⃧►iʑυвöτ◃2.0▹'
-global.namechannel2 = 'Zeus Bot🔆Channel-OFC'
-global.namegrupo = '🤖⃧►iʑυвöτ◃2.0▹'
-global.namecomu = '𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪 & ➲౽໋ⲉⷡυⷪ᥉ࣰ֧ⷮ✰᮫݄݃🪴'
-global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
-global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/ojqdd0.jpg')
-
-global.canalIdM = ["120363285614743024@newsletter", "120363395205399025@newsletter"]
-global.canalNombreM = ["🤖⃧►iʑυвöτ◃2.0▹", "Zeus Bot🔆Channel-OFC"]
-global.channelRD = await getRandomChannel()
-
-global.d = new Date(new Date + 3600000)
-global.locale = 'es'
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
-
-global.rwait = '🕒'
-global.done = '✅'
-global.error = '✖️'
-global.msm = '⚠︎'
-
-global.emoji = '🪴'
-global.emoji2 = '🍁'
-global.emoji3 = '🍎'
-global.emoji4 = '⚡'
-global.emoji5 = '🌱'
-global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
-global.e1 = '🪴'
-global.e2 = '🍁'
-global.e3 = '🍎'
-global.e4 = '⚡'
-global.e5 = '🌱'
-global.e = [e1, e2, e3, e4, e5].getRandom()
-global.s1 = '➪'
-global.s2 = '➺'
-global.s3 = '➣'
-global.s4 = '⬭'
-global.s5 = '⬖'
-global.s = [s1, s2, s3, s4, s5].getRandom()
-global.wait = '❍ Espera un momento, soy lenta...';
-global.waitt = '❍ Espera un momento, soy lenta...';
-global.waittt = '❍ Espera un momento, soy lenta...';
-global.waitttt = '❍ Espera un momento, soy lenta...';
-
-/*var canal = 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H'
-var comunidad = 'https://chat.whatsapp.com/HHDvYPActKSDNgMB8bBJ9G'
-var ig = 'https://www.instagram.com/edar504__'
-var github = 'https://www.instagram.com/edar504__'
-let correo = 'izumilitee@gmail.com'*/
-let c = 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H'
-let g = 'https://chat.whatsapp.com/E7FwA25TmvS2ncJragtbYV'
-let i = 'https://www.instagram.com/edar504__'
-let t = 'https://www.tiktok.com/@edar_xd'
-global.redes = [c, g, i, t].getRandom()
-
-/*let category = "imagen"
-const db = './src/database/db.json'
-const db_ = JSON.parse(fs.readFileSync(db))
-const random = Math.floor(Math.random() * db_.links[category].length)
-const randomlink = db_.links[category][random]
-const response = await fetch(randomlink)
-const rimg = await response.buffer()
-global.icons = rimg*/
-
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
-global.saludo = hour;
-
-global.nombre = m.pushName || 'Anónimo'
-global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-var more = String.fromCharCode(8206)
-global.readMore = more.repeat(850)
-
-global.packsticker = `${nombre}`;
-global.packsticker2 = `${wm}`
-  
-global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
-global.meta = {
-  key: {
-    participant: '0@s.whatsapp.net',
-    remoteJid: 'status@broadcast',
-    fromMe: false,
-    id: 'MetaAI'
-  },
-  message: {
-    contactMessage: {
-      displayName: '🫩Meta AI',
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nN:AI;Meta;;;\nFN:Meta AI\nitem1.TEL;waid=13135550002:13135550002\nitem1.X-ABLabel:Soporte\nEND:VCARD`,
-      jpegThumbnail: null,
-      thumbnail: null,
-      sendEphemeral: true
-    }
-  },
-  participant: '0@s.whatsapp.net'
-};
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
-}}, { quoted: m }
-
-global.icono = [
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me2.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me3.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me4.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me5.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me6.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me7.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me8.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me9.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me10.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me11.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me12.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me13.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me14.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me15.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me16.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me17.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me18.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me19.jpg',
-  'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me20.jpg'
-].getRandom()
-
-const thumbnail = await (await fetch(icono)).buffer()
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: false, title: wm, body: textbot, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: redes, sourceUrl: redes, thumbnail, mediaType: 1, renderLargerThumbnail: false }, }, }}
-
-export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-  }
-
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 async function getRandomChannel() {
-let randomIndex = Math.floor(Math.random() * canalIdM.length)
-let id = canalIdM[randomIndex]
-let name = canalNombreM[randomIndex]
-return { id, name }
+  const canalIdM = ["120363285614743024@newsletter", "120363395205399025@newsletter"]
+  const canalNombreM = ["🤖⃧►iʑυвöτ◃2.0▹", "Zeus Bot🔆Channel-OFC"]
+
+  const idx = Math.floor(Math.random() * canalIdM.length)
+  return {
+    id: canalIdM[idx],
+    name: canalNombreM[idx]
+  }
+}
+
+export async function before(m, { conn }) {
+  try {
+    global.emoji = '🪴'
+    global.emoji2 = '🍁'
+    global.emoji3 = '🍎'
+    global.emoji4 = '⚡'
+    global.emoji5 = '🌱'
+    global.emojis = getRandom([global.emoji, global.emoji2, global.emoji3, global.emoji4, global.emoji5])
+
+    global.e1 = '🪴'
+    global.e2 = '🍁'
+    global.e3 = '🍎'
+    global.e4 = '⚡'
+    global.e5 = '🌱'
+    global.e = getRandom([global.e1, global.e2, global.e3, global.e4, global.e5])
+
+    global.s1 = '➪'
+    global.s2 = '➺'
+    global.s3 = '➣'
+    global.s4 = '⬭'
+    global.s5 = '⬖'
+    global.s = getRandom([global.s1, global.s2, global.s3, global.s4, global.s5])
+
+    let c = 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H'
+    let g = 'https://wa.me/50492280729?text=Hola+quiero+un+bot+para+mi+grupo,+cuáles+son+los+planes?+'
+    let i = 'https://www.instagram.com/edar504__'
+    let t = 'https://www.tiktok.com/@edar_xd'
+    global.redes = getRandom([c, g, i, t])
+
+    global.canalIdM = ["120363285614743024@newsletter", "120363395205399025@newsletter"]
+    global.canalNombreM = ["🤖⃧►iʑυвöτ◃2.0▹", "Zeus Bot🔆Channel-OFC"]
+    global.channelRD = await getRandomChannel() // ahora definida arriba
+
+    global.fake = {
+      contextInfo: {
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: global.channelRD.id,
+          newsletterName: global.channelRD.name,
+          serverMessageId: -1
+        }
+      },
+      quoted: m
+    }
+    
+    const iconosArray = [
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me2.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me3.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me4.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me5.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me6.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me7.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me8.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me9.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me10.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me11.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me12.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me13.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me14.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me15.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me16.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me17.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me18.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me19.jpg',
+      'https://raw.githubusercontent.com/CheirZ/Repo-img/main/zeus-jpeg/me20.jpg'
+    ]
+    global.icono = getRandom(iconosArray)
+    const thumbnailBuffer = await (await fetch(global.icono)).buffer()
+
+    const wm = global.wm || 'IzuBot'
+    const textbot = global.textbot || 'Bienvenido'
+
+    global.rcanal = {
+      contextInfo: {
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: global.channelRD.id,
+          serverMessageId: 100,
+          newsletterName: global.channelRD.name
+        },
+        externalAdReply: {
+          showAdAttribution: false,
+          title: wm,
+          body: textbot,
+          mediaUrl: null,
+          description: null,
+          previewType: "PHOTO",
+          thumbnailUrl: global.redes,
+          sourceUrl: global.redes,
+          thumbnail: thumbnailBuffer,
+          mediaType: 1,
+          renderLargerThumbnail: false
+        }
+      }
+    }
+
+  } catch (err) {
+    console.error('before() error:', err)
+    throw err
+  }
 }
