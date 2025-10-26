cb60ff7a96ade38bc6540722f3e51122772daf17
gudo1234 <izumilitee@gmail.com>
2025-10-26 00:46:33 -0600

    Update _ds.js

--- plugins/_ds.js
+++ plugins/_ds.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 
 const directoryPath = `./${jadi}/`;
 const sanSessionPath = `./${sessions}/`;
-const tempPath = './temp/'; // Ruta de la carpeta temp
 
 function cleanSubbotDirectories() {
   fs.readdir(directoryPath, (err, subbotDirs) => {
@@ -44,27 +43,9 @@ function cleanSessionFiles() {
   });
 }
 
-function cleanTempPNGs() {
-  fs.readdir(tempPath, (err, files) => {
-    if (err) return console.log('No se puede escanear la carpeta temp: ' + err);
-
-    files.forEach((file) => {
-      if (file.endsWith('.png')) {
-        fs.unlink(`${tempPath}${file}`, (err) => {
-          if (err && err.code !== 'ENOENT') {
-            console.log(`Error al eliminar PNG: ${file}: ` + err);
-          }
-        });
-      }
-    });
-  });
-}
-
 const handler = async (m, { conn }) => {
   cleanSubbotDirectories();
   cleanSessionFiles();
-  cleanTempPNGs();
-  //m.reply('¡Sesiones y archivos .png limpiados correctamente!');
 };
 
 handler.customPrefix = /😂|😁|🤣|😅|😆|😎|🤖|👾|❤️/;
