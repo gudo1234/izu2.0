dca1c6bfe61704bf78e280d3ad539cc5fd355f0f
gudo1234 <izumilitee@gmail.com>
2025-10-29 08:20:13 -0600

    Update _queso.js

--- plugins/_queso.js
+++ plugins/_queso.js
@@ -21,6 +21,6 @@ let handler = async (m, { conn }) => {
   )
 }
 
-handler.customPrefix = /^(e|a|q|que|qe|ke|Qe|k|Ke|Kee|Quee|pe|p)$/i
+handler.customPrefix = /^(e|a|q|que|qe|ke|Qe|k|Ke|Kee|Quee|pe)$/i
 handler.command = new RegExp
 export default handler
