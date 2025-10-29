ef989ba3d0bbd15a1a6aa5e966958a4dee3c2098
gudo1234 <izumilitee@gmail.com>
2025-10-29 08:56:05 -0600

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,14 +1,13 @@
 const handler = async (m, { conn }) => {
-try {
-const start = Date.now();
-const sentMsg = await conn.sendMessage(m.chat, { text: Fetching... }, { quoted: m });
-const end = Date.now();
-await conn.sendMessage(m.chat, { text: Response:\n> ${end - start}ms, edit: sentMsg.key });
-} catch (e) {
-m.reply(Error en plugin "ping":\n${e.message});
-}
+  try {
+    const start = Date.now();
+    const sentMsg = await conn.sendMessage(m.chat, { text: `Fetching...` }, { quoted: m });
+    const end = Date.now();
+    await conn.sendMessage(m.chat, { text: `*Response:*\n> ${end - start}ms`, edit: sentMsg.key });
+  } catch (e) {
+    m.reply(`Error en plugin "ping":\n${e.message}`);
+  }
 }
 
-handler.customPrefix = /^(p|ping)$/i
-handler.command = new RegExp
-export default handler
+handler.command = ['p', 'ping'];
+export default handler;
