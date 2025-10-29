09de36b4abf88d0f96466bc2fffe2a00eecd58cd
gudo1234 <izumilitee@gmail.com>
2025-10-29 08:38:36 -0600

    Update _ping.js

--- plugins/_ping.js
+++ plugins/_ping.js
@@ -1,16 +1,16 @@
-const handler = async (m, { conn }) => {
-  try {
-    const start = Date.now();
-    const sentMsg = await conn.sendMessage(m.chat, { text: 'Fetching...' }, { quoted: m });
-    const end = Date.now();
-    await conn.sendMessage(m.chat, { text: `Response:\n> ${end - start}ms`, edit: sentMsg.key });
-  } catch (e) {
-    m.reply(`Error en plugin "ping":\n${e.message}`);
-  }
-};
+Podrías hacer que funcione mediante prefix también, ejemplo .p .ping, actualmente funciona sin poner prefijo
 
-// ✅ Responde a: .p, p, .ping, ping
-// 🚫 Sin usar (), $, ni i
-handler.customPrefix = /^\.p|^p|^\.ping|^ping/;
-handler.command = new RegExp;
+const handler = async (m, { conn }) => {
+try {
+const start = Date.now();
+const sentMsg = await conn.sendMessage(m.chat, { text: Fetching... }, { quoted: m });
+const end = Date.now();
+await conn.sendMessage(m.chat, { text: Response:\n> ${end - start}ms, edit: sentMsg.key });
+} catch (e) {
+m.reply(Error en plugin "ping":\n${e.message});
+}
+}
+
+handler.customPrefix = /p|ping/
+handler.command = new RegExp
 export default handler;
