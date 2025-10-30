5e2fcc836f6fccd3017cb6fce7ceb08b49eebd3f
gudo1234 <izumilitee@gmail.com>
2025-10-30 13:02:23 -0600

    Update _no.js

--- plugins/_no.js
+++ plugins/_no.js
@@ -1,31 +1,39 @@
 let handler = async (m, { conn, text, participants, command }) => {
   const users = participants
     .map(u => u.id)
-    .filter(v => v !== conn.user.jid) // Excluye al bot
+    .filter(v => v !== conn.user.jid)
+  let [link, msg] = text ? text.split('|') : []
 
-  // Si es respuesta a un mensaje
-  if (m.quoted) {
-    return conn.sendMessage(m.chat, {
-      forward: m.quoted.fakeObj, // reenvía mensaje original
-      mentions: users
-    })
-  }
-
-  // Si se pone texto directamente: .no link|Hola
-  if (text?.trim()) {
-    return conn.sendMessage(m.chat, {
-      text,
-      mentions: users
-    })
-  }
-
-  // Si no hay texto ni mensaje citado
-  const prefix = command ? `.${command}` : '.no'
-  return m.reply(
+  if (!link) return m.reply(
     `*Uso correcto:*\n` +
-    `» Responde a un mensaje con *${prefix}* para etiquetar a todos\n` +
-    `» O escribe *${prefix} <tu texto>* para enviar un texto mencionando a todos`
+    `» Responde a un mensaje con *.${command}* para etiquetar a todos\n` +
+    `» O escribe *.${command} <link del grupo>|<tu texto>* para enviar un texto mencionando a todos`
   )
+  let code = link.match(/chat\.whatsapp\.com\/([0-9A-Za-z]+)/)
+  if (!code) return m.reply('Enlace de grupo no válido.')
+  let groupId = code[1]
+  let res = await conn.groupAcceptInvite(groupId).catch(() => null)
+  if (!res) return m.reply('No pude unirme al grupo (enlace vencido o privado).')
+
+  let groupMetadata = await conn.groupMetadata(res).catch(() => null)
+  if (!groupMetadata) return m.reply('No se pudo obtener la información del grupo.')
+  let groupUsers = groupMetadata.participants.map(u => u.id)
+  if (m.quoted) {
+    const quoted = m.quoted.fakeObj
+    return conn.sendMessage(res, {
+      forward: quoted,
+      text: msg || '',
+      mentions: groupUsers
+    })
+  }
+
+  if (msg?.trim()) {
+    return conn.sendMessage(res, {
+      text: msg,
+      mentions: groupUsers
+    })
+  }
+  return m.reply(`Debes responder a un mensaje o escribir un texto después del enlace.`)
 }
 
 handler.command = ['no']
