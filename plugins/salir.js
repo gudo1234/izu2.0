a5683000a989dafa0924a9e2ef69162d680bcc84
gudo1234 <izumilitee@gmail.com>
2025-11-01 00:41:37 -0600

    Update salir.js

--- plugins/salir.js
+++ plugins/salir.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, command }) => {
   let fechaHoraMX = moment().tz('America/Mexico_City').locale('es').format('dddd D [de] MMMM [del] YYYY [a las] h:mm a [hora México]')
 
  let txt = `${e} \`Saliendo automáticamente del grupo...\`\n*Nombre:* ${groupMetadata.subject}\n*ID:* ${id}\n> ${fechaHoraMX}`
-  await conn.sendButton(m.chat, txt, textbot, icono, [], null, [['ᴏᴡɴᴇʀ/true', nu], ['sᴇɢᴜɪʀ ᴄᴀɴᴀʟ', channel]], m)
+  await conn.sendButton(m.chat, txt, textbot, icono, [], null, [['ᴏᴡɴᴇʀ/true', `https://wa.me/50492280729?text=Hola+quiero+un+bot+para+mi+grupo,+cuáles+son+los+planes?+`], ['sᴇɢᴜɪʀ ᴄᴀɴᴀʟ', channel]], m)
 
   await new Promise(resolve => setTimeout(resolve, 3000))
 
