8f087f3b344113e5899bfc5f66f4ebf01ac57d32
gudo1234 <izumilitee@gmail.com>
2025-11-01 23:17:19 -0600

    Update _unbanchat.js

--- plugins/_unbanchat.js
+++ plugins/_unbanchat.js
@@ -1,10 +1,14 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, `${e} Bot activo en este grupo.`, m, rcanal)
-    await m.react('✅')
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '〽️l🔥 *¡Este chat no está registrado!*', m, rcanal)
+let chat = global.db.data.chats[m.chat]
+if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡ᴇʟ ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
+chat.isBanned = false
+await conn.reply(m.chat, '⚡ *¡ᴇʟ ʙᴏᴛ ʏᴀ ғᴜᴇ ᴅᴇsʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, rcanal)
 }
 
-handler.command = ['desbanearbot', 'unbanchat']
-handler.group = true 
+handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.admin = true 
+handler.botadmin = true
+handler.group = true
+
 export default handler
