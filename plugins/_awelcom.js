2a6afb746847ef11d6c36b2b42a1861febc5bb88
gudo1234 <izumilitee@gmail.com>
2025-11-02 02:35:10 -0600

    Update _awelcom.js

--- plugins/_awelcom.js
+++ plugins/_awelcom.js
@@ -38,7 +38,8 @@ let handler = async (m, { conn, __dirname }) => {
 
   try {
     // --- Imagen miniatura ---
-    const imgPath = join(__dirname, 'https://files.catbox.moe/njyrrp.jpg')
+    let pr = await (await fetch(`https://files.catbox.moe/njyrrp.jpg`)).buffer()
+    const imgPath = join(__dirname, pr)
     const thumbLocal = fs.existsSync(imgPath) ? fs.readFileSync(imgPath) : null
     const thumbResized = thumbLocal
       ? await (await Jimp.read(thumbLocal)).resize(300, 150).getBufferAsync(Jimp.MIME_JPEG)
