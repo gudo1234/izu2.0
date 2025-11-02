c81a70713158b56e2f4fa784292ac41e8d0410f9
gudo1234 <izumilitee@gmail.com>
2025-11-01 21:56:45 -0600

    Update _ai.js

--- plugins/_ai.js
+++ plugins/_ai.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn, __dirname }) => {
 
   try {
     // --- Imagen miniatura ---
-    const imgPath = join(__dirname, '../thumbnail.jpg')
+    const imgPath = join(__dirname, 'https://files.catbox.moe/njyrrp.jpg')
     const thumbLocal = fs.existsSync(imgPath) ? fs.readFileSync(imgPath) : null
     const thumbResized = thumbLocal
       ? await (await Jimp.read(thumbLocal)).resize(300, 150).getBufferAsync(Jimp.MIME_JPEG)
