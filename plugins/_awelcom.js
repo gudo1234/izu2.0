e767d3792560700dbe184367fdf422f9dddd6e24
gudo1234 <izumilitee@gmail.com>
2025-11-02 18:30:16 -0600

    Update _awelcom.js

--- plugins/_awelcom.js
+++ plugins/_awelcom.js
@@ -33,7 +33,7 @@ try {
   let run = clockString(uptime)
 
   //documentImg
-  const imgPath = join(__dirname, '../thumbnail.jpg')
+  const imgPath = join(__dirname, 'https://files.catbox.moe/njyrrp.jpg')
     const thumbLocal = fs.existsSync(imgPath) ? fs.readFileSync(imgPath) : null
     const thumbResized = thumbLocal
       ? await (await Jimp.read(thumbLocal)).resize(300, 150).getBufferAsync(Jimp.MIME_JPEG)
