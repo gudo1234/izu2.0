10f91be51fc684c661b1e63d8e7cb07a3a8100b4
gudo1234 <izumilitee@gmail.com>
2025-11-14 14:43:18 -0600

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,23 +1,17 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
     try {
-        await m.react('🕒')
-        await m.reply(`${e} Reiniciando el bot...\n> Esto tomará unos segundos...`)
-        setTimeout(() => {
-            process.exit(0)
-        }, 3000)
+        await m.react('🕒');
+        await m.reply(`${e} Reiniciando El Bot...\n> Esto tomará unos segundos`);
+        setTimeout(() => process.exit(0), 3000);
     } catch (error) {
-        console.error(error)
-        conn.reply(m.chat, `${error}`, m)
     }
 }
 
-handler.command = ['restart', 'reiniciar', 'res']
-handler.rowner = true
+handler.command = ['restart', 'reiniciar', 'res'];
+handler.rowner = true;
 
-export default handler
-
-// 🕒 Reinicio automático cada 40 minutos
-setInterval(() => {
-    console.log('♻️ Reinicio automático cada 40 minutos...')
-    process.exit(0)
-}, 30 * 60 * 1000) // 40 minutos
+export default handler;
+process.on('unhandledRejection', () => {});
+process.on('uncaughtException', () => {});
+process.on('SIGINT', () => process.exit(0));
+process.on('SIGTERM', () => process.exit(0));
