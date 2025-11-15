cfa86ca9c2ff3f114dd12de6e6948f0f3919be5b
gudo1234 <izumilitee@gmail.com>
2025-11-15 11:41:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
 import './plugins/_allfake.js'
-import router from './router.js'
+//import router from './router.js'
 import cfonts from 'cfonts'
 import { createRequire } from 'module'
 import { fileURLToPath, pathToFileURL } from 'url'
