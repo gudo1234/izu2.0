323f8e424d6b84e3d6cedd66131a1a8230c84100
gudo1234 <izumilitee@gmail.com>
2025-11-15 11:42:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
 
-await router()
+//await router()
 
 say('Izu', {
 font: 'simple',
