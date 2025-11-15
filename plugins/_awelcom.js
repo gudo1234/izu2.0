e6a571b586d51430b565a183a2633c05dd3cf215
Ell Contrerax <sarmientodieg@gmail.com>
2025-11-15 13:27:33 -0500

    Actualizar _awelcom.js

--- plugins/_awelcom.js
+++ plugins/_awelcom.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { join } from 'path'
-import Jimp from 'jimp'
+import * as Jimp from 'jimp';
 import fetch from 'node-fetch'
 import { execSync } from 'child_process'
 import path from 'path'
