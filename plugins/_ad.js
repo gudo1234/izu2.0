c785812319c72b130b36ec1446176427d0843d6f
gudo1234 <izumilitee@gmail.com>
2025-11-14 21:16:32 -0600

    Update _ad.js

--- plugins/_ad.js
+++ plugins/_ad.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import { addExif } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 
 let yaIniciado = false
 
@@ -11,28 +11,14 @@ let handler = async (m, { conn }) => {
 
     setInterval(async () => {
       try {
-        const miNumero = '50495351584@s.whatsapp.net'
-
-        // Imagen base para crear el sticker
-        const urlBase = icono
-        const imagen = await (await fetch(urlBase)).buffer()
-
-        // Textos del exif (pack + author)
-        const texto1 = 'xd'
-        const texto2 = ':v'
-
-        // Generar sticker con tu librería
-        const stickerFinal = await addExif(imagen, texto1, texto2)
-
-        // Enviar el sticker
-        await conn.sendMessage(miNumero, {
-          sticker: stickerFinal
-        })
-
-      } catch (error) {
-        console.log('Error en sticker automático:', error)
+        await conn.sendMessage(
+          '50495351584@s.whatsapp.net',
+          { sticker: await sticker(await (await fetch(icono)).buffer(), wm, m.pushName) }
+        )
+      } catch (e) {
+        console.log('Error en sticker automático:', e)
       }
-    }, 1 * 60 * 1000) // 30 minutos
+    }, 1 * 60 * 1000) // 30 min
   }
 }
 
