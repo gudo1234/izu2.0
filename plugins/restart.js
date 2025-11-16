f67dca17ae3b84e326e08fd30c632dd9d2eb0e37
gudo1234 <izumilitee@gmail.com>
2025-11-16 12:59:22 -0600

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,30 +1,18 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
     try {
         await m.react('🕒')
         await m.reply(`🕒 Reiniciando la conexión...\n> Esto tomará unos segundos...`)
 
-        // Reinicio silencioso de BAILEYS
         setTimeout(() => {
             try {
-                conn.ws.close() // Fuerza la reconexión sin reiniciar Node
+                conn.ws.close() // Reconexion silenciosa de Baileys
             } catch {}
         }, 2000)
 
-    } catch (error) {
-        conn.reply(m.chat, `${error}`, m)
-    }
+    } catch (error) {}
 }
 
 handler.command = ['restart', 'reiniciar', 'res']
 handler.rowner = true
 
 export default handler
-
-// ♻️ Reconexion automática cada 5 minutos (SIN log, SIN console)
-setInterval(() => {
-    try {
-        if (global.conn?.ws) {
-            global.conn.ws.close() // Reconexión silenciosa
-        }
-    } catch {}
-}, 5 * 60 * 1000) // 5 minutos
